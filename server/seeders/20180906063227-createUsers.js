27f4080c47cd17137138b6e07dc69e3c9dfb0247
Ingvar <igor@wirnex.com>
2019-03-08 01:03:57 +0300

    restore migration

--- server/seeders/20180906063227-createUsers.js
+++ server/seeders/20180906063227-createUsers.js
@@ -1,10 +1,11 @@
-const models = require("../src/models");
-import _ from "lodash";
+'use strict';
+const models = require("../src/models")
+import _ from "lodash"
+import { getArgumentValues } from "graphql/execution/values";
 
-const users = [
-  {
-    name: "Goe Roggins"
-  },
+const users = [{
+  name: "Goe Roggins"
+},
   {
     name: "Bill Holliday"
   },
@@ -14,13 +15,12 @@ const users = [
   {
     name: "Paul Smith"
   }
-];
+]
 
-const bots = [
-  {
-    name: "FooBot",
-    competitorType: "COMPETITIVE",
-  },
+const bots = [{
+  name: "FooBot",
+  competitorType: "COMPETITIVE",
+},
   {
     name: "Bot 2",
     competitorType: "COMPETITIVE",
@@ -33,13 +33,12 @@ const bots = [
     name: "Bot 4",
     competitorType: "AGGREGATION",
   }
-];
+]
 
-const measurables = [
-  {
-    name: "Rating of usefulness of X",
-    valueType: "FLOAT"
-  },
+const measurables = [{
+  name: "Rating of usefulness of X",
+  valueType: "FLOAT"
+},
   {
     name: "Rating of usefulness of Y",
     valueType: "FLOAT"
@@ -58,90 +57,66 @@ const measurables = [
   }
 ]
 
-let uuid = 1000000;
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
 
-    try {
-      return;
-      const xxx = await models.User.create({
-        id: "1010a04d-5307-4bd0-9246-00000000",
-        name: "initial",
-      });
-      return;
-      const yyy = await models.Bot.create({
-        id: "1010a04d-5307-4bd0-9246-000000000000",
-        userId: "1010a04d-5307-4bd0-9246-000000000000",
-        name: "aggbot1"
-      });
-      return;
-      const zzz = await models.Agent.create({
-        id: "1010a04d-5307-4bd0-9256-00000000",
-        type: 'USER',
-      });
-      return;
-      let allUsers = [];
-      for (let user of users) {
-        const u = await models.User.create({
-          ...user,
-          id: uuid++,
-        });
-        allUsers = [...allUsers, u];
-      }
+    const xxx = await models.User.create({id: "1010a04d-5307-4bd0-9246-000000000000", name: "initial"})
+    const yyy = await models.Bot.create({id: "1010a04d-5307-4bd0-9246-000000000000", userId: "1010a04d-5307-4bd0-9246-000000000000", name: "aggbot1"});
 
-      let allBots = [];
-      for (let bot of bots) {
-        let bb = {...bot, userId: allUsers[0].dataValues.id, description: "sdf"};
-        const b = await models.Bot.create(bb);
-        allBots = [...allBots, b]
-      }
-
-      let allMeasurables = [];
-      for (let m of measurables) {
-        const u = await models.Measurable.create({ ...m, creatorId: zzz.id });
-        allMeasurables = [...allMeasurables, u];
-      }
-
-      let measurements = _.times(50, () => 0);
-      for (let m of measurements) {
-        let user = _.sample(allUsers)
-        let agent = await user.getAgent()
-        let measurable = _.sample(allMeasurables)
-        const u = await models.Measurement.create({
-          agentId: agent.id,
-          measurableId: measurable.id,
-          isAggregation: false,
-          competitorType: "COMPETITIVE",
-          value: {trio: {p25: 10.5, p50: 40.5, p75: 100.5 }}
-        })
-      }
-
-      for (let m of measurements) {
-        let bot = _.sample(allBots)
-        let agent = await bot.getAgent()
-        let measurable = _.sample(allMeasurables)
-        const u = await models.Measurement.create({
-          agentId: agent.id,
-          measurableId: measurable.id,
-          isAggregation: false,
-          competitorType: "COMPETITIVE",
-          value: {trio: {p25: 12.5, p50: 43.5, p75: 130.5 }}
-        })
-      }
-    } catch (e) {
-      console.error(e);
+    let allUsers = []
+    for (let user of users) {
+      const u = await models.User.create(user)
+      allUsers = [...allUsers, u]
     }
 
+    let allBots = []
+    for (let bot of bots) {
+      let bb = {...bot, userId: allUsers[0].dataValues.id, description: "sdf"}
+      const b = await models.Bot.create(bb)
+      allBots = [...allBots, b]
+    }
+
+    let allMeasurables = []
+    for (let m of measurables) {
+      const u = await models.Measurable.create(m)
+      allMeasurables = [...allMeasurables, u]
+    }
+
+    let measurements = _.times(50, () => 0);
+    for (let m of measurements) {
+      let user = _.sample(allUsers)
+      let agent = await user.getAgent()
+      let measurable = _.sample(allMeasurables)
+      const u = await models.Measurement.create({
+        agentId: agent.id,
+        measurableId: measurable.id,
+        isAggregation: false,
+        competitorType: "COMPETITIVE",
+        value: {trio: {p25: 10.5, p50: 40.5, p75: 100.5 }}
+      })
+    }
+
+    for (let m of measurements) {
+      let bot = _.sample(allBots)
+      let agent = await bot.getAgent()
+      let measurable = _.sample(allMeasurables)
+      const u = await models.Measurement.create({
+        agentId: agent.id,
+        measurableId: measurable.id,
+        isAggregation: false,
+        competitorType: "COMPETITIVE",
+        value: {trio: {p25: 12.5, p50: 43.5, p75: 130.5 }}
+      })
+    }
   },
 
-    down: async (queryInterface, Sequelize) => {
-      /*
-        Add reverting commands here.
-        Return a promise to correctly handle asynchronicity.
+  down: async (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
 
-        Example:
-        return queryInterface.bulkDelete('Person', null, {});
-      */
-    }
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
 };

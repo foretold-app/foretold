435b55dfbd6233498c9fc4adfdaf970c506858da
RomanGalochkin <roman.redrat@gmail.com>
2019-03-12 10:48:23 +0300

    Removes Dist directory, update .gitignore

--- server/dist/lib/urls.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-const clientUrl = exports.clientUrl = process.env.NODE_ENV === "development" ? "http://localhost:1234" : "https://www.foretold.io";
-//# sourceMappingURL=urls.js.map

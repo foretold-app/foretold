c8ca7e3026866162d58165ca90413422edd7171f
RomanGalochkin <roman.redrat@gmail.com>
2019-03-21 15:08:03 +0300

    Add test - 6

--- server/src/data/channels-data.spec.js
+++ server/src/data/channels-data.spec.js
@@ -15,6 +15,8 @@ describe('ChannelsData', () => {
   });
 
   const instance = new ChannelsData();
+  const user = {agentId: 'agentId1'};
+  const input = { name: 'Input Name 1'};
 
   describe('createOne - branch A', () => {
     beforeAll(() => {
@@ -23,12 +25,26 @@ describe('ChannelsData', () => {
       jest.spyOn(AgentsChannelsData.prototype, 'createOne').mockReturnValue(Promise.resolve(true));
     });
     it('createOne', () => {
-      const user = {agentId: 'agentId1'};
-      const input = { name: 'Input Name 1'};
       return instance.createOne(user, input).then((result) => {
         expect(models.Channel.findOne).toHaveBeenCalledWith({where: { name: input.name }});
         expect(models.Channel.create).toHaveBeenCalledTimes(0);
         expect(AgentsChannelsData.prototype.createOne).toHaveBeenCalledTimes(0);
+        expect(result).toBe(true);
+      });
+    });
+  });
+
+  describe('createOne - branch B', () => {
+    beforeAll(() => {
+      jest.spyOn(models.Channel, 'findOne').mockReturnValue(Promise.resolve(false));
+      jest.spyOn(models.Channel, 'create').mockReturnValue(Promise.resolve(true));
+      jest.spyOn(AgentsChannelsData.prototype, 'createOne').mockReturnValue(Promise.resolve(true));
+    });
+    it('createOne', () => {
+      return instance.createOne(user, input).then((result) => {
+        expect(models.Channel.findOne).toHaveBeenCalledWith({where: { name: input.name }});
+        expect(models.Channel.create).toHaveBeenCalledTimes(1);
+        expect(AgentsChannelsData.prototype.createOne).toHaveBeenCalledTimes(1);
         expect(result).toBe(true);
       });
     });

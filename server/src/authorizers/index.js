7815cf4988e560346ae386d3822c3b05895085b9
RomanGalochkin <roman.redrat@gmail.com>
2019-03-26 15:17:22 +0300

    PR Fixes - 2

--- server/src/authorizers/index.js
+++ server/src/authorizers/index.js
@@ -4,34 +4,12 @@ const { isChannelAllowed } = require('./channels');
 
 const permissions = shield({
   Query: {
-    // user: not(isAuthenticated),
-    // users: not(isAuthenticated),
     measurement: isChannelAllowed,
     measurements: isChannelAllowed,
     measurable: isChannelAllowed,
     measurables: isChannelAllowed,
-    // bot: not(isAuthenticated),
-    // bots: not(isAuthenticated),
-    // agent: not(isAuthenticated),
-    // agents: not(isAuthenticated),
     series: isChannelAllowed,
     seriesCollection: isChannelAllowed,
-    // channel: not(isAuthenticated),
-    // channels: not(isAuthenticated),
-    // stats: not(isAuthenticated),
-  },
-  Mutation: {
-    // createMeasurement: isAuthenticated,
-    // createMeasurable: isAuthenticated,
-    // createSeries: isAuthenticated,
-    // archiveMeasurable: isAuthenticated,
-    // unArchiveMeasurable: isAuthenticated,
-    // editMeasurable: isAuthenticated,
-    // editUser: isAuthenticated,
-    // channelUpdate: isAuthenticated,
-    // channelCreate: isAuthenticated,
-    // agentsChannelsCreate: isAuthenticated,
-    // agentsChannelsDelete: isAuthenticated,
   },
 });
 

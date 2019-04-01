81dd5789d408848598ec4441207e28eff39c998d
Roman Galochkin <roman.redrat@gmail.com>
2019-04-01 11:52:27 +0300

    Finalize migration

--- server/migrations/20190401001122-renameTable.js
+++ server/migrations/20190401001122-renameTable.js
@@ -1,9 +1,35 @@
 module.exports = {
   up: async function (queryInterface) {
     await queryInterface.renameTable('AgentsChannels', 'ChannelsMemberships');
+
+    await queryInterface.sequelize.query(`
+        ALTER TABLE "ChannelsMemberships" DROP CONSTRAINT "AgentsChannels_channelId_fkey";
+        ALTER TABLE "ChannelsMemberships" DROP CONSTRAINT "AgentsChannels_agentId_fkey";
+        
+        ALTER TABLE "ChannelsMemberships" ADD CONSTRAINT "ChannelsMemberships_channelId_fkey" 
+        FOREIGN KEY ("channelId") REFERENCES "Channels" ("id") 
+        on update cascade on delete set null;
+        
+        ALTER TABLE "ChannelsMemberships" ADD CONSTRAINT "ChannelsMemberships_agentId_fkey" 
+        FOREIGN KEY ("agentId") REFERENCES "Agents" ("id") 
+        on update cascade on delete set null;
+    `);
   },
 
   down: async function (queryInterface) {
-    await queryInterface.renameTable('ChannelsMemberships', 'AgentsChannels',);
+    await queryInterface.renameTable('ChannelsMemberships', 'AgentsChannels');
+
+    await queryInterface.sequelize.query(`
+        ALTER TABLE "AgentsChannels" DROP CONSTRAINT "ChannelsMemberships_channelId_fkey";
+        ALTER TABLE "AgentsChannels" DROP CONSTRAINT "ChannelsMemberships_agentId_fkey";
+        
+        ALTER TABLE "AgentsChannels" ADD CONSTRAINT "AgentsChannels_channelId_fkey" 
+        FOREIGN KEY ("channelId") REFERENCES "Channels" ("id") 
+        on update cascade on delete set null;
+        
+        ALTER TABLE "AgentsChannels" ADD CONSTRAINT "AgentsChannels_agentId_fkey" 
+        FOREIGN KEY ("agentId") REFERENCES "Agents" ("id") 
+        on update cascade on delete set null;
+    `);
   }
 };

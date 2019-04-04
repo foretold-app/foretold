198e2dd2575787458afbeab35a82f8e7f5b9f08f
RomanGalochkin <roman.redrat@gmail.com>
2019-04-04 16:59:29 +0300

    Fixes migration

--- server/migrations/20190404001122-rmClns.js
+++ server/migrations/20190404001122-rmClns.js
@@ -1,5 +1,5 @@
 module.exports = {
-  up: async function (queryInterface) {
+  up: async function (queryInterface, Sequelize) {
     await queryInterface.removeColumn('Measurables', 'isJudged');
     await queryInterface.removeColumn('Measurables', 'hasResolutionEndpointResolved');
     await queryInterface.addColumn("Measurables", "isArchived", {
@@ -9,7 +9,7 @@ module.exports = {
     });
   },
 
-  down: async function (queryInterface) {
+  down: async function (queryInterface, Sequelize) {
     await queryInterface.addColumn("Measurables", "isJudged", {
       type: Sequelize.BOOLEAN,
       allowNull: false,

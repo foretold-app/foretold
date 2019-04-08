8c4f64d319ebb0b202844c47c00a3e6306e53f96
RomanGalochkin <roman.redrat@gmail.com>
2019-04-08 10:18:52 +0300

    Fixes tests

--- server/src/resolvers/users.spec.js
+++ server/src/resolvers/users.spec.js
@@ -5,7 +5,7 @@ describe('Users Resolvers', () => {
 
   describe('users()', () => {
     const root = {};
-    const args = { id: 'id1', name: 'name1' };
+    const args = { id: 'id1', input: { name: 'name1' } };
     const context = { user: { agentId: 'agentId1' } };
     beforeEach(() => {
       jest.spyOn(data.users, 'updateOne').mockReturnValue(

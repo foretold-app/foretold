072e6774a3508b31fbe39ae00f1ef4b0028e3997
Ozzie Gooen <OAGr@users.noreply.github.com>
2019-04-16 16:23:41 +0100

    compoutId -> compoudId

--- server/src/middlewares/channel-memberships.js
+++ server/src/middlewares/channel-memberships.js
@@ -18,11 +18,11 @@ async function channelMemberships(root, args, context, info) {
     || _.get(context, 'channel.id');
   const agentId = _.get(context, 'agent.id');
 
-  const compountId = { agentId, channelId };
-  console.log('\x1b[36m ---> \x1b[0m Middleware (channelMemberships)', compountId);
+  const compoundId = { agentId, channelId };
+  console.log('\x1b[36m ---> \x1b[0m Middleware (channelMemberships)', compoundId);
 
   if (channelId && agentId) {
-    const channelMembership = await data.channelMemberships.getOne(compountId);
+    const channelMembership = await data.channelMemberships.getOne(compoundId);
     context.channelMembership = channelMembership;
     context.channelMembershipsRole = _.get(channelMembership, 'role', []);
   } else {

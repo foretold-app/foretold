15a3f2742f9362d90e9b304d779d0a2ba3b2e2f8
RomanGalochkin <roman.redrat@gmail.com>
2019-04-17 17:32:46 +0300

    Adds transactions for migrations, always do the same

--- server/migrations/20190417001111-removes-bots-without-users.js
+++ server/migrations/20190417001111-removes-bots-without-users.js
@@ -1,9 +1,19 @@
 module.exports = {
-  up: async function (queryInterface) {
-    // Warning
-    await queryInterface.bulkDelete('Bots', {
-      where: { userId: null },
-    });
+  up: async function (queryInterface, Sequelize) {
+    try {
+      await queryInterface.sequelize.query(`BEGIN`);
+
+      // Warning
+      await queryInterface.bulkDelete('Bots', {
+        userId: { [Sequelize.Op.eq]: null }
+      });
+
+      await queryInterface.sequelize.query(`COMMIT`);
+    } catch (e) {
+      await queryInterface.sequelize.query(`ROLLBACK`);
+      console.error(e);
+      throw e;
+    }
   },
 
   down: async function (queryInterface) {
--- server/migrations/20190417001122-renames-bot-columns.js
+++ server/migrations/20190417001122-renames-bot-columns.js
@@ -1,71 +1,91 @@
 module.exports = {
   up: async function (queryInterface, Sequelize) {
-    await queryInterface.removeConstraint('Bots', 'Bots_agentId_fkey');
-    await queryInterface.removeConstraint('Bots', 'Bots_userId_fkey');
+    try {
+      await queryInterface.sequelize.query(`BEGIN`);
 
-    await queryInterface.renameColumn('Bots', 'userId', 'ownerUserId');
-    await queryInterface.renameColumn('Bots', 'agentId', 'ownerAgentId');
+      await queryInterface.removeConstraint('Bots', 'Bots_agentId_fkey');
+      await queryInterface.removeConstraint('Bots', 'Bots_userId_fkey');
 
-    await queryInterface.addConstraint('Bots', ['ownerUserId'], {
-      type: 'foreign key',
-      name: 'Bots_ownerUserId_fkey',
-      references: {
-        table: 'Users',
-        field: 'id'
-      },
-      onDelete: 'cascade',
-      onUpdate: 'cascade'
-    });
+      await queryInterface.renameColumn('Bots', 'userId', 'ownerUserId');
+      await queryInterface.renameColumn('Bots', 'agentId', 'ownerAgentId');
 
-    await queryInterface.addConstraint('Bots', ['ownerAgentId'], {
-      type: 'foreign key',
-      name: 'Bots_ownerAgentId_fkey',
-      references: {
-        table: 'Agents',
-        field: 'id'
-      },
-      onDelete: 'cascade',
-      onUpdate: 'cascade'
-    });
+      await queryInterface.addConstraint('Bots', ['ownerUserId'], {
+        type: 'foreign key',
+        name: 'Bots_ownerUserId_fkey',
+        references: {
+          table: 'Users',
+          field: 'id'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      });
 
-    await queryInterface.changeColumn('Bots', 'ownerUserId', {
-      type: Sequelize.DataTypes.UUIDV4,
-      allowNull: false,
-    });
+      await queryInterface.addConstraint('Bots', ['ownerAgentId'], {
+        type: 'foreign key',
+        name: 'Bots_ownerAgentId_fkey',
+        references: {
+          table: 'Agents',
+          field: 'id'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      });
+
+      await queryInterface.changeColumn('Bots', 'ownerUserId', {
+        type: Sequelize.DataTypes.UUID(),
+        allowNull: false,
+      });
+
+      await queryInterface.sequelize.query(`COMMIT`);
+    } catch (e) {
+      await queryInterface.sequelize.query(`ROLLBACK`);
+      console.error(e);
+      throw e;
+    }
   },
 
-  down: async function (queryInterface) {
-    await queryInterface.removeConstraint('Bots', 'Bots_ownerUserId_fkey');
-    await queryInterface.removeConstraint('Bots', 'Bots_ownerAgentId_fkey');
+  down: async function (queryInterface, Sequelize) {
+    try {
+      await queryInterface.sequelize.query(`BEGIN`);
 
-    await queryInterface.renameColumn('Bots', 'ownerUserId', 'userId');
-    await queryInterface.renameColumn('Bots', 'ownerAgentId', 'agentId');
+      await queryInterface.removeConstraint('Bots', 'Bots_ownerUserId_fkey');
+      await queryInterface.removeConstraint('Bots', 'Bots_ownerAgentId_fkey');
 
-    await queryInterface.addConstraint('Bots', ['userId'], {
-      type: 'foreign key',
-      name: 'Bots_userId_fkey',
-      references: {
-        table: 'Users',
-        field: 'id'
-      },
-      onDelete: 'cascade',
-      onUpdate: 'cascade'
-    });
+      await queryInterface.renameColumn('Bots', 'ownerUserId', 'userId');
+      await queryInterface.renameColumn('Bots', 'ownerAgentId', 'agentId');
 
-    await queryInterface.addConstraint('Bots', ['agentId'], {
-      type: 'foreign key',
-      name: 'Bots_agentId_fkey',
-      references: {
-        table: 'Agents',
-        field: 'id'
-      },
-      onDelete: 'cascade',
-      onUpdate: 'cascade'
-    });
+      await queryInterface.addConstraint('Bots', ['userId'], {
+        type: 'foreign key',
+        name: 'Bots_userId_fkey',
+        references: {
+          table: 'Users',
+          field: 'id'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      });
 
-    await queryInterface.changeColumn('Bots', 'userId', {
-      type: Sequelize.DataTypes.UUIDV4,
-      allowNull: true,
-    });
+      await queryInterface.addConstraint('Bots', ['agentId'], {
+        type: 'foreign key',
+        name: 'Bots_agentId_fkey',
+        references: {
+          table: 'Agents',
+          field: 'id'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      });
+
+      await queryInterface.changeColumn('Bots', 'userId', {
+        type: Sequelize.DataTypes.UUID(),
+        allowNull: true,
+      });
+
+      await queryInterface.sequelize.query(`COMMIT`);
+    } catch (e) {
+      await queryInterface.sequelize.query(`ROLLBACK`);
+      console.error(e);
+      throw e;
+    }
   }
 };

726a9298c3a7e3485c6d0ebd222faeb6bcc3c855
RomanGalochkin <roman.redrat@gmail.com>
2019-04-17 13:54:54 +0300

    Removes obsolete logic

--- server/src/models-abstract/channel-membership-model.js
+++ server/src/models-abstract/channel-membership-model.js
@@ -21,26 +21,13 @@ class ChannelMembershipModel extends ModelPostgres {
    * @returns {Promise<Models.ChannelMemberships>}
    */
   async updateOne(channelId, agentId, role) {
-    const transaction = await this.getTransaction();
-    try {
-      const channelMembership = await this.model.findOne({
-        where: { channelId, agentId },
-        transaction
-      });
-      if (channelMembership) {
-        await channelMembership.update({
-          role,
-        }, {
-          transaction
-        });
-        await this.checkCountOfAdmins(channelId, transaction);
-        await transaction.commit();
-      }
-      return channelMembership;
-    } catch (e) {
-      await transaction.rollback();
-      return Promise.reject(e);
+    const channelMembership = await this.model.findOne({
+      where: { channelId, agentId },
+    });
+    if (channelMembership) {
+      await channelMembership.update({ role });
     }
+    return channelMembership;
   }
 
   /**
@@ -50,47 +37,15 @@ class ChannelMembershipModel extends ModelPostgres {
    * @returns {Promise<Models.ChannelMemberships | null>}
    */
   async deleteOne(channelId, agentId) {
-    const transaction = await this.getTransaction();
-    try {
-      const channelMembership = await this.model.findOne({
-        where: { channelId, agentId },
-        transaction
-      });
-      if (channelMembership) {
-        await this.model.destroy({
-          where: { channelId, agentId },
-          transaction,
-        });
-        await this.checkCountOfAdmins(channelId, transaction);
-        await transaction.commit();
-      }
-      return channelMembership;
-    } catch (e) {
-      await transaction.rollback();
-      return Promise.reject(e);
+    const channelMembership = await this.model.findOne({
+      where: { channelId, agentId },
+    });
+    if (channelMembership) {
+      await channelMembership.destroy();
     }
+    return channelMembership;
   }
 
-  /**
-   * @protected
-   * @param {string} channelId
-   * @param {object} transaction
-   * @return {Promise<*>}
-   */
-  async checkCountOfAdmins(channelId, transaction) {
-    const countOfAdmins = await this.model.count({
-      where: {
-        channelId,
-        role: ChannelMembershipModel.ROLES.ADMIN,
-      },
-      transaction,
-    });
-    if (countOfAdmins === 0) {
-      // @todo: move into permissions
-      throw new Error('Operation is not permitted.');
-    }
-    return countOfAdmins;
-  }
 }
 
 ChannelMembershipModel.ROLES = CHANNEL_MEMBERSHIP_ROLES;

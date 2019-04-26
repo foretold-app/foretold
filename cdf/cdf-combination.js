4f09044bbfb22da98d5734c410b7342d40a7e9b0
RomanGalochkin <roman.redrat@gmail.com>
2019-04-26 15:57:04 +0300

    Adds primary interfaces

--- cdf/cdf-combination.js
+++ cdf/cdf-combination.js
@@ -1,37 +1,62 @@
 const R = require('ramda');
 
 const { CDF } = require('./cdf');
-const { range } = require('./functions');
+const { range, min, max } = require('./functions');
 
 class CdfCombination {
+  /**
+   * @param {CDF[]} cdfs
+   */
   constructor(cdfs) {
     this.cdfs = cdfs;
   }
 
+  /**
+   * @return {number}
+   */
   minBounds() {
     return min(R.map(e => e.xs[0], this.cdfs))
   }
 
+  /**
+   * @return {number}
+   */
   maxBounds() {
     return max(R.map(e => e.xs[0], this.cdfs))
   }
 
+  /**
+   * @param {number} sampleCount
+   * @return {number[]}
+   */
   xsToAnalyze(sampleCount) {
     return range(this.minBounds(), this.maxBounds(), sampleCount);
   }
 
+  /**
+   * @param {number} xPoint
+   * @return {number[]}
+   */
   allYsAtXPoint(xPoint) {
     return R.map(r => r.findY(xPoint), this.cdfs);
   }
 
-  // This assumes equal weighting, which is fine for now
+  /**
+   * This assumes equal weighting, which is fine for now
+   * @param {number} xPoint
+   * @return {number}
+   */
   meanOfYsAtXPoint(xPoint) {
-    return R.mean(this.allCdfYsAtXPoint);
+    return R.mean(this.allYsAtXPoint(xPoint));
   }
 
+  /**
+   * @param {number} sampleCount
+   * @return {CDF}
+   */
   combine(sampleCount) {
     const xs = this.xsToAnalyze(sampleCount);
-    const means = R.map(r => this.meanOfYsAtXPoint(xPoint), xs);
+    const means = R.map(xPoint => this.meanOfYsAtXPoint(xPoint), xs);
     return new CDF(xs, means);
   }
 }
--- cdf/functions.js
+++ cdf/functions.js
@@ -1,12 +1,11 @@
 const R = require('ramda');
 
 /**
- *
- * @param xMin
- * @param xMax
- * @param yMin
- * @param yMax
- * @param xIntended
+ * @param {number} xMin
+ * @param {number} xMax
+ * @param {number} yMin
+ * @param {number} yMax
+ * @param {number} xIntended
  * @return {number}
  */
 function interpolate(xMin, xMax, yMin, yMax, xIntended) {
@@ -18,9 +17,15 @@ function interpolate(xMin, xMax, yMin, yMax, xIntended) {
 const min = R.reduce(R.min, Infinity);
 const max = R.reduce(R.max, -Infinity);
 
-//TODO: This should return an array of n evenly-spaced items between min and max, including min and max.
-// range(1,5,3) = [1, 3, 5];
-// range(1,5,5) = [1, 2, 3, 4, 5];
+/**
+ * TODO: This should return an array of n evenly-spaced items between min and max, including min and max.
+ * range(1,5,3) = [1, 3, 5];
+ * range(1,5,5) = [1, 2, 3, 4, 5];
+ * @param {number} min
+ * @param {number} max
+ * @param {number} n
+ * @return {number[]}
+ */
 function range(min, max, n) {
   // const increment = (maxBounds - minBounds)/n;
 }

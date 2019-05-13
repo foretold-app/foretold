f8b72eb1e6da5c0701356e29176249a873ab2622
RomanGalochkin <roman.redrat@gmail.com>
2019-05-13 14:28:16 +0300

    Fixes reference type

--- server/migrations/20190513142600-tokens-table.js
+++ server/migrations/20190513142600-tokens-table.js
@@ -11,7 +11,7 @@ module.exports = {
           allowNull: false,
         },
         agentId: {
-          type: Sequelize.DataTypes.STRING,
+          type: Sequelize.DataTypes.UUID(),
           allowNull: false,
           references: {
             model: 'Agents',

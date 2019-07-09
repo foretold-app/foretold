86665d85bf45e5a1f361be5c85475f118321c04d
RomanGalochkin <roman.redrat@gmail.com>
2019-07-09 14:45:35 +0300

    Adds migration

---
+++ packages/server/migrations/201907090948001-email-verifies-column.js
@@ -0,0 +1,23 @@
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+    try {
+      await queryInterface.addColumn('Users', 'isEmailVerified', {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      });
+    } catch (e) {
+      console.error('Migration Up Error', e);
+      throw e;
+    }
+  },
+
+  down: async function (queryInterface) {
+    try {
+      await queryInterface.removeColumn('Users', 'isEmailVerified');
+    } catch (e) {
+      console.error('Migration Down Error', e);
+      throw e;
+    }
+  }
+};

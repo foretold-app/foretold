04ff10dc107906e011ac29194ddf5789d3e5b48c
RomanGalochkin <roman.redrat@gmail.com>
2019-07-22 13:15:38 +0300

    Adds migration for new system event

---
+++ packages/server/migrations/201907220048001-template-measurable-resolved.js
@@ -0,0 +1,50 @@
+const { TEMPLATE_NAME } = require('../src/models/enums/template-name');
+const { EmailEnvelopeTemplate } = require('../src/models/classes/templates');
+
+module.exports = {
+  up: async function (queryInterface) {
+    try {
+      await queryInterface.sequelize.query(`BEGIN`);
+      const envelopeTemplate = new EmailEnvelopeTemplate({
+        subject: 'Question "{{ measurable.name }}" was resolved',
+        body:
+          'The question [{{ measurable.name }}] was resolved. ' +
+          'Click here to see the result.',
+      });
+
+
+      const values = {
+        envelopeTemplate: JSON.stringify(envelopeTemplate),
+      };
+
+      const cond = {
+        name: TEMPLATE_NAME.MEASURABLE_STATE_IS_RESOLVED,
+      };
+
+      await queryInterface.bulkUpdate('Templates', values, cond);
+
+      await queryInterface.sequelize.query(`COMMIT`);
+    } catch (e) {
+      console.error('Migration Up Error', e);
+      await queryInterface.sequelize.query(`ROLLBACK`);
+      throw e;
+    }
+  },
+
+  down: async function (queryInterface) {
+
+    try {
+      await queryInterface.sequelize.query(`BEGIN`);
+
+      await queryInterface.bulkDelete('Templates', {
+        name: TEMPLATE_NAME.MEASURABLE_STATE_IS_RESOLVED,
+      });
+
+      await queryInterface.sequelize.query(`COMMIT`);
+    } catch (e) {
+      console.error('Migration Down Error', e);
+      await queryInterface.sequelize.query(`ROLLBACK`);
+      throw e;
+    }
+  }
+};

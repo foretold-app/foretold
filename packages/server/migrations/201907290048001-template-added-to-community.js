c7083c8de4b78f299c8ef1f46f325454609e939b
Roman Galochkin <roman.redrat@gmail.com>
2019-07-29 16:29:56 +0300

    Adds new template for "Member Added to a Community"

---
+++ packages/server/migrations/201907290048001-template-added-to-community.js
@@ -0,0 +1,49 @@
+const { TEMPLATE_NAME } = require('../src/models/enums/template-name');
+const { EmailEnvelopeTemplate } = require('../src/models/classes/templates');
+
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+    try {
+      await queryInterface.sequelize.query(`BEGIN`);
+
+      const envelopeTemplate = new EmailEnvelopeTemplate({
+        subject: 'You is added to the Foretold community',
+        body:
+          '<a href="{{{ agent.link }}}">{{ agent.name }}</a> has ' +
+          'added you to the Foretold community, ' +
+          '[<a href="{{{ measurable.link }}}">{{ measurable.name }}</a>]'
+      });
+
+      await queryInterface.bulkInsert('Templates', [{
+        id: Sequelize.fn('uuid_generate_v4'),
+        envelopeTemplate: JSON.stringify(envelopeTemplate),
+        name: TEMPLATE_NAME.MEMBER_ADDED_TO_COMMUNITY,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }]);
+
+      await queryInterface.sequelize.query(`COMMIT`);
+    } catch (e) {
+      console.error('Migration Up Error', e);
+      await queryInterface.sequelize.query(`ROLLBACK`);
+      throw e;
+    }
+  },
+
+  down: async function (queryInterface) {
+
+    try {
+      await queryInterface.sequelize.query(`BEGIN`);
+
+      await queryInterface.bulkDelete('Templates', {
+        name: TEMPLATE_NAME.MEMBER_ADDED_TO_COMMUNITY,
+      });
+
+      await queryInterface.sequelize.query(`COMMIT`);
+    } catch (e) {
+      console.error('Migration Down Error', e);
+      await queryInterface.sequelize.query(`ROLLBACK`);
+      throw e;
+    }
+  }
+};

0b297dad129ab207dc55d53ad6cee19c56c34e67
RomanGalochkin <roman.redrat@gmail.com>
2019-07-30 15:52:43 +0300

    Adds comments to help in future in reading code base

--- packages/server/src/models/hooks.js
+++ packages/server/src/models/hooks.js
@@ -4,6 +4,26 @@ const emitter = require('../async/emitter');
 const { AGENT_TYPE } = require('./enums/agent-type');
 const { MEASUREMENT_COMPETITOR_TYPE } = require('./enums/measurement-competitor-type');
 
+/**
+ * Try to keep all "async" hooks in one place.
+ * For what do you want to ask?
+ *
+ * Server side application architecture is being building with separated,
+ * decomposed layers (Model Definitions, Models Abstractions, Data Layer.
+ * Middleware Layer, Authorizers Layer, Async Flows Layer, GrqphQL
+ * Layer ans its resolvers).
+ *
+ * Models are just "data type definitions" which are similar with "grapqhl
+ * types". "Sequelize" is a just lib which plays role of "DB Driver".
+ *
+ * So this is why we keep "async" part of the application here, and
+ * "associations" definitions too. It does not matter where the "associations"
+ * are defined and placed (either DB or JS files). The main purpose
+ * is to give to developers to read this base definitions without
+ * duplications and keep in a mind only a small part of this.
+ *
+ */
+
 function addHooks(db) {
   db.sequelize.addHook('afterUpdate', (instance) => {
     if (

efb976937d9e8d4139b084397990981ae83e522d
RomanGalochkin <roman.redrat@gmail.com>
2019-08-02 11:46:55 +0300

    Tuns off Invite Feature for a short time

--- packages/server/src/async/producers/member-invited-to-community.js
+++ packages/server/src/async/producers/member-invited-to-community.js
@@ -1,11 +1,26 @@
 const { Producer } = require('./producer');
-const { MemberToCommunity } = require('./member-to-community');
 
-class MemberInvitedToCommunity extends MemberToCommunity {
-  constructor(invitation = {}) {
-    super(invitation);
+class MemberInvitedToCommunity extends Producer {
+  constructor(input = {}) {
+    super({});
+
+    this.input = input;
     this.templateName = Producer.TEMPLATE_NAME.MEMBER_INVITED_TO_COMMUNITY;
   }
+
+  /**
+   * @public
+   * @return {Promise<boolean>}
+   */
+  async main() {
+    try {
+      return true;
+    } catch (e) {
+      console.log(`MemberInvitedToCommunity`, e.message, e);
+      return false;
+    }
+  }
+
 }
 
 module.exports = {

177947ad5bc17a58c9151cc0149f18551856043f
RomanGalochkin <roman.redrat@gmail.com>
2019-08-12 15:51:05 +0300

    Fast refactoring

--- packages/cdf/samples.js
+++ packages/cdf/samples.js
@@ -1,47 +1,71 @@
-const { interpolate, range, min, max, random } = require('./functions');
-const {percentile, sortDescending} = require('./dataAnalysis.js');
-const pdfast = require('pdfast')
-const {Pdf} = require ('./cdf')
+const pdfast = require('pdfast');
+
+const { percentile, sortDescending } = require('./dataAnalysis.js');
+const { Pdf } = require('./pdf');
 
 class Samples {
   /**
-   * @param {number[]} xs
-   * @param {number[]} ys
+   * @param samples
    */
   constructor(samples) {
-      this.samples = samples;
-      this.sorted = [];
+    this.samples = samples;
+    this.sorted = [];
   }
 
-  sort(){
+  sort() {
     this.sorted = sortDescending(this.samples);
   }
 
-  getPercentile(perc){
+  getPercentile(perc) {
     const length = this.sorted.length;
-    if (length === 0){
+    if (length === 0) {
       console.error("Samples Percentile: You must sort samples before finding the percentile.")
     }
     return percentile(this.sorted, length, perc);
   }
 
-  toPdf({min, max, size, width}){
-    let args = {size, width};
-    if (!!min) { args.min = min };
-    if (!!max) { args.max = max };
+  /**
+   * @param min
+   * @param max
+   * @param size
+   * @param width
+   * @return {Pdf|*}
+   */
+  toPdf({ min, max, size, width }) {
+    let args = { size, width };
+
+    if (!!min) args.min = min;
+    if (!!max) args.max = max;
+
     let kde = this._kde(args);
+    console.log("KDE Samples", this.samples);
+    console.log("KDE", kde);
     let pdf = new Pdf(kde.map(r => r.x), kde.map(r => r.y));
     return pdf;
   }
 
-  toCdf({min, max, size, width}){
-      let pdf = this.toPdf({min, max, size, width});
-      let cdf = pdf.toCdf();
-      return cdf;
+  /**
+   * @param min
+   * @param max
+   * @param size
+   * @param width
+   * @return {Cdf}
+   */
+  toCdf({ min, max, size, width }) {
+    let pdf = this.toPdf({ min, max, size, width });
+    let cdf = pdf.toCdf();
+    return cdf;
   }
 
-  _kde({min, max, size, width}){
-      return pdfast.create(this.samples, {min, max, size, width});
+  /**
+   * @param min
+   * @param max
+   * @param size
+   * @param width
+   * @private
+   */
+  _kde({ min, max, size, width }) {
+    return pdfast.create(this.samples, { min, max, size, width });
   }
 
 }
--- packages/client/src/lib/cdfInput/GuesstimateInput.js
+++ packages/client/src/lib/cdfInput/GuesstimateInput.js
@@ -1,77 +1,111 @@
 import React from "react";
-import {Guesstimator} from '@foretold/guesstimator/index';
-import {Input} from "antd";
-import {Samples} from "@foretold/cdf";
+import { Guesstimator } from '@foretold/guesstimator/index';
+import { Input } from "antd";
+import { Samples } from "../../../../cdf";
 import _ from "lodash"
 
+/**
+ * @param ratio
+ * @return {string}
+ */
 const minMaxRatio = (ratio) => {
-    if (ratio < 100000){
-        return "SMALL"
-    } else if (ratio < 10000000){
-        return "MEDIUM"
-    } else {
-        return "LARGE"
-    }
+  if (ratio < 100000) {
+    return "SMALL"
+  } else if (ratio < 10000000) {
+    return "MEDIUM"
+  } else {
+    return "LARGE"
+  }
 };
 
+/**
+ * @param samples
+ * @return {string}
+ */
 let ratioSize = samples => {
-    samples.sort();
-    const minValue = samples.getPercentile(2);
-    const maxValue = samples.getPercentile(98);
-    return minMaxRatio(maxValue/minValue);
-}
+  samples.sort();
+  const minValue = samples.getPercentile(2);
+  const maxValue = samples.getPercentile(98);
+  return minMaxRatio(maxValue / minValue);
+};
 
+/**
+ * @param values
+ * @param min
+ * @param max
+ * @return {*[]}
+ */
 const toCdf = (values, min, max) => {
-    let _values = values;
-    if (_.isFinite(min)){_values = _.filter(_values, r => r > min)};
-    if (_.isFinite(max)){_values = _.filter(_values, r => r < max)};
-    const samples = new Samples(_values);
-    const width = ratioSize(samples) == "SMALL" ? 20 : 1;
-    const cdf = samples.toCdf({size:1000, width, min, max});
-    return [cdf.ys, cdf.xs, ratioSize == "LARGE"];
+  let _values = values;
+  if (_.isFinite(min)) _values = _.filter(_values, r => r > min);
+  if (_.isFinite(max)) _values = _.filter(_values, r => r < max);
+
+  console.log("values toCdf", { _values });
+  const samples = new Samples(_values);
+  console.log("toCdf::samples", { samples });
+
+  const ratioSize$ = ratioSize(samples);
+  const width = ratioSize$ === "SMALL" ? 20 : 1;
+  console.log("toCdf::width", { width });
+
+  const cdf = samples.toCdf({ size: 1000, width, min, max });
+  console.log("toCdf::toCdf",{ size: 1000, width, min, max });
+  console.log("toCdf::cdf", { cdf });
+
+  return [cdf.ys, cdf.xs, ratioSize$ === "LARGE"];
 };
 
 export class GuesstimateInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: '', items:[]};
-        this.handleChange = this.handleChange.bind(this);
-        this.textInput = React.createRef();
+  /**
+   * @param props
+   */
+  constructor(props) {
+    super(props);
+    this.state = { value: '', items: [] };
+    this.handleChange = this.handleChange.bind(this);
+    this.textInput = React.createRef();
+  }
+
+  componentDidMount() {
+    if (this.props.focusOnRender) {
+      this.textInput.focus();
+    }
+  }
+
+  handleChange(event) {
+    const text = event.target.value;
+
+    let [error, item] = Guesstimator.parse({ text });
+    console.log("A", { error, item });
+    let parsedInput = item.parsedInput;
+    console.log("B", { parsedInput });
+    let what = new Guesstimator({ parsedInput: parsedInput });
+    console.log("c", { what });
+    let foo = what.sample(this.props.sampleCount);
+    console.log("D", { foo });
+    let values = _.filter(foo.values, _.isFinite);
+    console.log("E", { values });
+
+    if (!!values) {
+      this.setState({ value: event.target.value, items: values });
+    } else {
+      this.setState({ value: event.target.value, items: [] });
     }
 
-    componentDidMount(){
-        if (this.props.focusOnRender){
-            this.textInput.focus();
-        }
+    if (!values) {
+      this.props.onUpdate([[], [], false]);
+    } else if (values.length === 1) {
+      this.props.onUpdate([[1], values, false]);
+    } else {
+      this.props.onUpdate(toCdf(values, this.props.min, this.props.max));
     }
+    this.props.onChange(text);
+  }
 
-    handleChange(event) {
-        const text = event.target.value;
-
-        let [error, item] = Guesstimator.parse({text});
-        let parsedInput = item.parsedInput;
-        let what = new Guesstimator({parsedInput: parsedInput});
-        let foo = what.sample(this.props.sampleCount);
-        let values = _.filter(foo.values, _.isFinite);
-
-        if (!!values) {
-            this.setState({value: event.target.value, items: values});
-        } else {
-            this.setState({value: event.target.value, items: []});
-        }
-
-        if (!values){
-            this.props.onUpdate([[], [], false]);
-        } else if (values.length === 1){
-            this.props.onUpdate([[1], values, false]);
-        } else {
-            this.props.onUpdate(toCdf(values, this.props.min, this.props.max));
-        }
-        this.props.onChange(text);
-      }
-    
-    render() {
-        return (<Input type="text" placeholder="10 to 100" value={this.state.value} onChange={this.handleChange} ref={input => this.textInput = input}/>
-        )
-    }
+  render() {
+    return (<Input type="text" placeholder="10 to 100" value={this.state.value}
+                   onChange={this.handleChange}
+                   ref={input => this.textInput = input}/>
+    )
+  }
 }

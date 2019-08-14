40d737cb1e52cba86ce9b309c004f8c3a75bd4c4
RomanGalochkin <roman.redrat@gmail.com>
2019-08-14 15:16:45 +0300

    Test GitHub checks

--- packages/server/src/github/api.js
+++ packages/server/src/github/api.js
@@ -1,86 +1,18 @@
-const _ = require('lodash');
-const graphqlClient = require('graphql-client');
+const GitHub = require('github-api');
 
-const queries = require('./queries');
-
-class API {
-  /**
-   * @param {string} token
-   */
-  constructor(token) {
-    this.token = token;
-    this.graphqlClient = graphqlClient;
-    this.queries = queries;
-    this.config = { API_URL: 'https://api.github.com/graphql' };
-    this.client = this.getClient();
-  }
-
-  /**
-   * @protected
-   * @return {{query}}
-   */
-  getClient() {
-    return this.graphqlClient({
-      url: this.config.API_URL,
-      headers: {
-        Authorization: 'Bearer ' + this.token,
-      }
-    });
-  }
-
-  /**
-   * @protected
-   * @param query
-   * @param {object} [variables]
-   * @return {*}
-   */
-  query(query, variables = {}) {
-    return this.client.query(query, variables, (req, res) => {
-      if (res.status === 401) {
-        throw new Error('Not authorized');
-      }
-    }).then((body) => {
-      // console.log(query, variables, body);
-      return body;
-    }).catch((err) => {
-      console.log('Query Error', err.message);
-      return Promise.reject(err);
-    });
-  }
-
-  /**
-   * @public
-   * @return {object}
-   */
-  async queryAuthenticated() {
-    const result = await this.query(this.queries.authenticated);
-    return this.getEntity('authenticated')(result);
-  }
-  /**
-   * @protected
-   * @param {string} alias
-   * @return {Function}
-   */
-  getEntity(alias) {
-    return (result) => {
-      this.proceedErrors(result);
-      return _.get(result, ['data', alias]);
-    }
-  }
-
-  /**
-   * @param result
-   */
-  proceedErrors(result) {
-    const errors = _.get(result, 'errors', []);
-    if (errors.length === 0) return;
-    _.each(errors, (err) => {
-      const message = _.get(err, 'message');
-      console.error(message);
-    });
+class API extends GitHub {
+  async getListOfHooks() {
+    const repo = await super.getRepo('foretold-app', 'ken-data');
+    const list = await repo.listHooks();
+    return list;
   }
 }
 
+const api = new API({
+  token: '0e869ed656bb78c73a085c1442bc6db75d110160'
+});
+
 module.exports = {
+  api,
   API,
 };
--- packages/server/src/github/app.js
+++ packages/server/src/github/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 
+const { api } = require('./api');
+
 app.get('/', (req, res, next) => {
   console.log('Get for Hooks App.');
-  res.send('OK');
+  // res.send('OK');
+  api.getListOfHooks().then((list) => {
+    res.send(list);
+  }).catch((err) => {
+    console.error(err);
+    res.send('ERR');
+  })
 });
 
 app.post('/', (req, res, next) => {

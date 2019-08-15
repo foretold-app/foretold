6c3e147d780168beb295080bf633736a0813bb37
RomanGalochkin <roman.redrat@gmail.com>
2019-08-15 15:45:01 +0300

    Experiment

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -35,16 +35,15 @@ app.use(cors());
 }
 
 {
+  const url = `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`;
   app.get('/env.js', (_req, res) => res.send(
     `window.ENV = { ` +
-    `API_URL: "${process.env.API_URL}", ` +
+    `API_URL: "${url}/graphql", ` +
     `AUTH0_DOMAIN: "${process.env.AUTH0_DOMAIN}", ` +
     `AUTH0_CLIENT_ID: "${process.env.AUTH0_CLIENT_ID}", ` +
-    `SERVER_URL: "${process.env.SERVER_URL}", ` +
     `}`
   ));
 }
-
 
 {
   const { app: subApp } = require('./github/app');

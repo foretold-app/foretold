61462260a4b41e1aaeec8ec65aeb19094846d604
RomanGalochkin <roman.redrat@gmail.com>
2019-08-15 15:12:59 +0300

    Adds default env object

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -40,6 +40,7 @@ app.use(cors());
     `API_URL: "${process.env.API_URL}", ` +
     `AUTH0_DOMAIN: "${process.env.AUTH0_DOMAIN}", ` +
     `AUTH0_CLIENT_ID: "${process.env.AUTH0_CLIENT_ID}", ` +
+    `SERVER_URL: "${process.env.SERVER_URL}", ` +
     `}`
   ));
 }

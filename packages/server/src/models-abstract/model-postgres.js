a8a2206bed548c27835b7c0aa535f20a4ba2bcf1
RomanGalochkin <roman.redrat@gmail.com>
2019-08-19 15:56:42 +0300

    Adds todos

--- packages/server/src/models-abstract/model-postgres.js
+++ packages/server/src/models-abstract/model-postgres.js
@@ -102,7 +102,7 @@ class ModelPostgres extends Model {
    * @return {string}
    */
   _channelIdsByMembers(agentId) {
-    return  `(
+    return `(
       SELECT "Channels"."id" FROM "Channels"
       LEFT OUTER JOIN 
         "ChannelMemberships" 
@@ -146,6 +146,7 @@ class ModelPostgres extends Model {
     if (restrictions.isAdmin) return where;
     if (!where[this.and]) where[this.and] = [];
 
+    // @todo: Rename it to "within Public Channels by c͟h͟a͟n͟n͟e͟l͟ ͟i͟d͟"
     if (restrictions.channelId && !restrictions.agentId) {
       where[this.and].push({
         channelId: {
@@ -154,6 +155,7 @@ class ModelPostgres extends Model {
       });
     }
 
+    // @todo: Rename it to "within Public and Joined Channels by c͟h͟a͟n͟n͟e͟l͟ ͟i͟d͟"
     if (restrictions.channelId && restrictions.agentId) {
       where[this.and].push({
         channelId: {
@@ -162,6 +164,7 @@ class ModelPostgres extends Model {
       });
     }
 
+    // @todo: Rename it to "within Public and Joined Channels by i͟d͟"
     if (restrictions.channelIdAsId && restrictions.agentId) {
       where[this.and].push({
         id: {
@@ -184,6 +187,7 @@ class ModelPostgres extends Model {
       });
     }
 
+    // @todo: Rename it to "within Joined Channels by c͟h͟a͟n͟n͟e͟l͟ ͟i͟d͟".
     if (restrictions.channelMemberId) {
       where[this.and].push({
         channelId: {
@@ -206,7 +210,7 @@ class ModelPostgres extends Model {
   applyRestrictionsIncluding(include = [], restrictions = {}) {
     if (!include) include = [];
 
-    // @todo: It is a filter, but not restriction
+    // @todo: It is a filter, b͟u͟t͟ ͟n͟o͟t͟ ͟r͟e͟s͟t͟r͟i͟c͟t͟i͟o͟n͟
     if (restrictions.measuredByAgentId) {
       include.push({
         model: this.models.Measurement,
@@ -229,14 +233,7 @@ class ModelPostgres extends Model {
     if (!where) where = {};
     if (!where[this.and]) where[this.and] = [];
 
-    if (filter.isArchived) {
-      where[this.and].push({
-        isArchived: {
-          [this.in]: this._getBooleansOfList(filter.isArchived),
-        }
-      });
-    }
-
+    // @todo: Rename it to "within Joined Channels by i͟d͟".
     if (filter.channelMemberId) {
       where[this.and].push({
         id: {
@@ -245,18 +242,30 @@ class ModelPostgres extends Model {
       });
     }
 
+    // OK
+    if (filter.isArchived) {
+      where[this.and].push({
+        isArchived: {
+          [this.in]: this._getBooleansOfList(filter.isArchived),
+        }
+      });
+    }
+
+    // OK
     if (filter.userId) {
       where[this.and].push({
         userId: filter.userId,
       });
     }
 
+    // OK
     if (filter.agentId) {
       where[this.and].push({
         agentId: filter.agentId,
       });
     }
 
+    // OK
     if (filter.channelId) {
       where[this.and].push({
         channelId: filter.channelId,

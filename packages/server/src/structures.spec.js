926b0f68954316ac796bf0d47d38fe0ecd0047f0
RomanGalochkin <roman.redrat@gmail.com>
2019-08-23 11:10:27 +0300

    Adds small tests for another functions

--- packages/server/src/structures.spec.js
+++ packages/server/src/structures.spec.js
@@ -1,5 +1,11 @@
 const index = require('./structures');
 const { withinMeasurables } = require('./structures');
+const { withinPublicChannelsById } = require('./structures');
+const { withinPublicChannelsByChannelId } = require('./structures');
+const { withinPublicAndJoinedChannelsById } = require('./structures');
+const { withinPublicAndJoinedChannelsByChannelId } = require('./structures');
+const { withinJoinedChannelsById } = require('./structures');
+const { withinJoinedChannelsByChannelId } = require('./structures');
 
 describe('Structure Factories', () => {
   it('should be', () => {
@@ -37,5 +43,35 @@ describe('Structure Factories', () => {
     );
     expect(withinMeasurables()).toEqual(null);
     expect(() => withinMeasurables('a')).toThrow();
+  });
+  it('withinPublicChannelsById()', () => {
+    expect(withinPublicChannelsById()).toEqual({as: 'id'});
+  });
+  it('withinPublicChannelsByChannelId()', () => {
+    expect(withinPublicChannelsByChannelId()).toEqual({as: 'channelId'});
+  });
+  it('withinPublicAndJoinedChannelsById()', () => {
+    expect(() => withinPublicAndJoinedChannelsById()).toThrow();
+    expect(withinPublicAndJoinedChannelsById('a')).toEqual({
+      as: 'id', agentId: 'a',
+    });
+  });
+  it('withinPublicAndJoinedChannelsByChannelId()', () => {
+    expect(() => withinPublicAndJoinedChannelsByChannelId()).toThrow();
+    expect(withinPublicAndJoinedChannelsByChannelId('a')).toEqual({
+      as: 'channelId', agentId: 'a',
+    });
+  });
+  it('withinJoinedChannelsById()', () => {
+    expect(() => withinJoinedChannelsById()).toThrow();
+    expect(withinJoinedChannelsById('a')).toEqual({
+      as: 'id', agentId: 'a',
+    });
+  });
+  it('withinJoinedChannelsByChannelId()', () => {
+    expect(() => withinJoinedChannelsByChannelId()).toThrow();
+    expect(withinJoinedChannelsByChannelId('a')).toEqual({
+      as: 'channelId', agentId: 'a',
+    });
   });
 });

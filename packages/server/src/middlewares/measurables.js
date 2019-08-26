44cd115c2466751b7c84268fdd1f47a9df17fea6
RomanGalochkin <roman.redrat@gmail.com>
2019-08-26 09:54:04 +0300

    Fixes creating action

--- packages/server/src/middlewares/measurables.js
+++ packages/server/src/middlewares/measurables.js
@@ -1,7 +1,10 @@
 const _ = require('lodash');
 
 const data = require('../data');
+
 const { Params } = require('../data/classes/params');
+const { Query } = require('../data/classes/query');
+const { Options } = require('../data/classes/options');
 
 /**
  * @param {object | null} root
@@ -16,12 +19,18 @@ async function setContextMeasurable(root, args, context, info) {
     || _.get(root, 'measurableId')
     || _.get(context, 'measurableId')
     || _.get(args, 'id');
+  const currentAgentId = _.get(context, 'agent.id');
 
-  console.log('\x1b[36m ---> \x1b[0m Middleware (measurable)', { measurableId });
+  console.log(
+    '\x1b[36m ---> \x1b[0m Middleware (measurable)',
+    { measurableId },
+  );
 
   if (measurableId) {
     const params = new Params({ id: measurableId });
-    context.measurable = await data.measurables.getOne(params);
+    const query = new Query();
+    const options = new Options({ agentId: currentAgentId });
+    context.measurable = await data.measurables.getOne(params, query, options);
   } else {
     context.measurable = null;
   }

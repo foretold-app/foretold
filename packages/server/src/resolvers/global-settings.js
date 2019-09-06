056bc77d63911cd1815fa299af052400560efa4f
RomanGalochkin <roman.redrat@gmail.com>
2019-09-06 11:08:41 +0300

    Adds mutation to manipulate with global settings

--- packages/server/src/resolvers/global-settings.js
+++ packages/server/src/resolvers/global-settings.js
@@ -1,9 +1,11 @@
 const _ = require('lodash');
 
 const data = require('../data');
+
 const { Params } = require('../data/classes/params');
 const { Options } = require('../data/classes/options');
 const { Query } = require('../data/classes/query');
+const { Data } = require('../data/classes/data');
 
 /**
  * @param {*} root
@@ -20,13 +22,35 @@ async function one(root, args, context, info) {
   const query = new Query();
   const options = new Options();
 
-  return await data.globalSettings.getOne(
+  return data.globalSettings.getOne(
     params,
     query,
     options,
   );
 }
 
+/**
+ * @param {*} root
+ * @param {object} args
+ * @param {Models.ObjectID} args.name
+ * @param {Models.ObjectID} args.input
+ * @param {object} args.input
+ * @param {Schema.Context} context
+ * @param {object} info
+ * @returns {Promise<*>}
+ */
+async function update(root, args, context, info) {
+  const name = _.get(args, 'name');
+  const input = _.get(args, 'input');
+
+  const params = new Params({ name });
+  const datas = new Data(input);
+  const options = new Options();
+
+  return data.globalSettings.updateOne(params, datas, options);
+}
+
 module.exports = {
   one,
+  update,
 };
--- packages/server/src/types/global-settings.js
+++ packages/server/src/types/global-settings.js
@@ -6,12 +6,22 @@ const globalSetting = new graphql.GraphQLObjectType({
   name: 'GlobalSetting',
   fields: () => ({
     id: { type: graphql.GraphQLNonNull(graphql.GraphQLString) },
+    name: { type: graphql.GraphQLString },
     entityGraph: { type: GraphQLJSON },
+    botAgentId: { type: graphql.GraphQLString },
     createdAt: { type: graphql.GraphQLNonNull(DateType.default) },
     updatedAt: { type: graphql.GraphQLNonNull(DateType.default) },
   })
 });
 
+const globalSettingUpdateInput = new graphql.GraphQLInputObjectType({
+  name: 'GlobalSettingUpdateInput',
+  fields: () => ({
+    botAgentId: { type: graphql.GraphQLString },
+  }),
+});
+
 module.exports = {
   globalSetting,
+  globalSettingUpdateInput,
 };

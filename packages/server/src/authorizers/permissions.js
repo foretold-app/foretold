d16d0bf70fa24ade00aead884660a7f315c79f80
RomanGalochkin <roman.redrat@gmail.com>
2019-09-06 11:12:41 +0300

    Adds authorizer

--- packages/server/src/authorizers/permissions.js
+++ packages/server/src/authorizers/permissions.js
@@ -51,6 +51,7 @@ const rulesChannel = {
       currentAgentIsAuthenticated,
       currentAgentIsApplicationAdminOrChannelAdmin,
     ),
+    globalSettingUpdate: currentAgentIsApplicationAdmin,
   }
 };
 

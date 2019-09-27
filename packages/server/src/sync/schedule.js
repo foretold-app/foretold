07d5398a20878b4ac03c25c3b45f71a759226860
RomanGalochkin <roman.redrat@gmail.com>
2019-09-27 11:50:00 +0300

    Fix 7

--- packages/server/src/sync/schedule.js
+++ packages/server/src/sync/schedule.js
@@ -7,35 +7,74 @@ const { listenFor } = require('./listeners');
 function runListeners() {
   console.log('Listeners are in a queue.');
   try {
-    emitter.on(events.SERVER_IS_READY, listeners.addGitHubWebHook);
+    emitter.on(events.SERVER_IS_READY,
+      listeners.addGitHubWebHook);
 
-    emitter.on(events.EVERY_HOUR, listeners.toJudgementPendingTransition);
-    emitter.on(events.EVERY_HOUR, listeners.toResolving);
-    emitter.on(events.EVERY_MINUTE, listeners.emailConsumer);
-    emitter.on(events.MAIL, listeners.mailer);
+    emitter.on(events.EVERY_HOUR,
+      listeners.toJudgementPendingTransition);
+    emitter.on(events.EVERY_HOUR,
+      listeners.toResolving);
+    emitter.on(events.EVERY_MINUTE,
+      listeners.emailConsumer);
+    emitter.on(events.MAIL,
+      listeners.mailer);
 
-    emitter.on(events.MEASURABLE_CHANGED, listenFor(producers.notifications.MeasurableStateChanged));
-    emitter.on(events.MEASURABLE_CHANGED, listenFor(producers.notifications.MeasurableStateResolved));
-    emitter.on(events.MEASURABLE_CHANGED, listenFor(producers.feedItems.NewMeasurableReachedResolution));
-    emitter.on(events.MEASURABLE_CHANGED, listeners.updateMeasurable);
+    /**
+     * MEASURABLE_CHANGED
+     */
+    emitter.on(events.MEASURABLE_CHANGED,
+      listenFor(producers.notifications.MeasurableStateChanged));
+    emitter.on(events.MEASURABLE_CHANGED,
+      listenFor(producers.notifications.MeasurableStateResolved));
+    emitter.on(events.MEASURABLE_CHANGED,
+      listenFor(producers.feedItems.NewMeasurableReachedResolution));
+    emitter.on(events.MEASURABLE_CHANGED,
+      listeners.updateMeasurable);
 
-    emitter.on(events.NEW_MEMBERSHIP, listenFor(producers.notifications.MemberAddedToCommunity));
-    emitter.on(events.NEW_MEMBERSHIP, listenFor(producers.notifications.MemberInvitedToCommunity));
-    emitter.on(events.NEW_MEMBERSHIP, listenFor(producers.feedItems.MemberJoinedCommunity));
+    /**
+     * NEW_MEMBERSHIP
+     */
+    emitter.on(events.NEW_MEMBERSHIP,
+      listenFor(producers.notifications.MemberAddedToCommunity));
+    emitter.on(events.NEW_MEMBERSHIP,
+      listenFor(producers.notifications.MemberInvitedToCommunity));
+    emitter.on(events.NEW_MEMBERSHIP,
+      listenFor(producers.feedItems.MemberJoinedCommunity));
 
-    emitter.on(events.NEW_MEASUREMENT, listenFor(producers.feedItems.NewMeasurementPrediction));
-    emitter.on(events.NEW_MEASUREMENT, listenFor(producers.feedItems.NewMeasurementComment));
-    emitter.on(events.NEW_MEASUREMENT, listenFor(producers.feedItems.NewMeasurementResolution));
-    emitter.on(events.NEW_MEASUREMENT, listenFor(producers.feedItems.NewMeasurementNotAvailable));
-    emitter.on(events.NEW_MEASUREMENT, listeners.newMeasurement);
-    emitter.on(events.NEW_MEASUREMENT, listeners.measurableStateTransition);
+    /**
+     * NEW_MEASUREMENT
+     */
+    emitter.on(events.NEW_MEASUREMENT,
+      listenFor(producers.feedItems.NewMeasurementPrediction));
+    emitter.on(events.NEW_MEASUREMENT,
+      listenFor(producers.feedItems.NewMeasurementComment));
+    emitter.on(events.NEW_MEASUREMENT,
+      listenFor(producers.feedItems.NewMeasurementResolution));
+    emitter.on(events.NEW_MEASUREMENT,
+      listenFor(producers.feedItems.NewMeasurementNotAvailable));
+    emitter.on(events.NEW_MEASUREMENT,
+      listeners.newMeasurement);
+    emitter.on(events.NEW_MEASUREMENT,
+      listeners.measurableStateTransition);
 
-    emitter.on(events.NEW_MEASURABLE, listenFor(producers.feedItems.NewMeasurable));
-    emitter.on(events.NEW_MEASURABLE, listeners.newMeasurable);
+    /**
+     * NEW_MEASURABLE
+     */
+    emitter.on(events.NEW_MEASURABLE,
+      listenFor(producers.feedItems.NewMeasurable));
+    emitter.on(events.NEW_MEASURABLE,
+      listeners.newMeasurable);
 
-    emitter.on(events.NEW_CHANNEL, listenFor(producers.feedItems.NewChannel));
-    emitter.on(events.NEW_CHANNEL, listeners.createChannelMembership);
-    emitter.on(events.NEW_SERIES, listeners.createNewMeasurables);
+    /**
+     * NEW_CHANNEL
+     */
+    emitter.on(events.NEW_CHANNEL,
+      listenFor(producers.feedItems.NewChannel));
+    emitter.on(events.NEW_CHANNEL,
+      listeners.createChannelMembership);
+
+    emitter.on(events.NEW_SERIES,
+      listeners.createNewMeasurables);
   } catch (e) {
     console.error('Listener error', e);
   }

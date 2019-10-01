dd507cbc8a10e60886de210aa1044de34e3ba47a
RomanGalochkin <roman.redrat@gmail.com>
2019-10-01 09:27:57 +0300

    Fixes tests

--- packages/server/src/data/notification-statuses-data.spec.js
+++ packages/server/src/data/notification-statuses-data.spec.js
@@ -1,7 +1,7 @@
 const data = require('./notification-statuses-data');
 
-describe('Agent Notifications Data', () => {
+describe('Notification Statuses Data', () => {
   it('class should be a constructor', () => {
-    expect(data.AgentNotificationsData).toBeInstanceOf(Function);
+    expect(data.NotificationStatusesData).toBeInstanceOf(Function);
   });
 });

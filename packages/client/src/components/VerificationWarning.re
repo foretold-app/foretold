dc6e40ed9cbff21f7752d3ae3e9c24154d23a59d
RomanGalochkin <roman.redrat@gmail.com>
2019-10-04 15:51:46 +0300

    Specifies email

--- packages/client/src/components/VerificationWarning.re
+++ packages/client/src/components/VerificationWarning.re
@@ -1,23 +1,54 @@
 let component = ReasonReact.statelessComponent("Banner");
+
+module Styles = {
+  open Css;
+  let icon = style([marginRight(`px(4))]);
+};
+
 let make = _ => {
   ...component,
   render: _ => {
-    let warning =
-      <>
-        {"Please verify your email address with Auth0.com and " |> Utils.ste}
-        <Link.Jsx2 linkType={Action(_ => Auth.Actions.logout())}>
-          {"relogin after" |> Utils.ste}
-        </Link.Jsx2>
-        {"." |> Utils.ste}
-      </>;
-    <Providers.AppContext.Consumer>
-      ...{({loggedInUser}) =>
-        switch (loggedInUser) {
-        | Some({isEmailVerified: Some(isEmailVerified)}) =>
-          isEmailVerified ? ReasonReact.null : warning
-        | _ => warning
-        }
-      }
-    </Providers.AppContext.Consumer>;
+    <>
+      <Providers.AppContext.Consumer>
+        ...{({loggedInUser}) => {
+          let warning = email =>
+            <>
+              <span className=Styles.icon>
+                <Icon.Icon icon="EMAIL_UNREAD" />
+              </span>
+              {"Please verify your email \""
+               ++ email
+               ++ "\" address in Auth0.com and "
+               |> Utils.ste}
+              <Link.Jsx2 linkType={Action(_ => Auth.Actions.logout())}>
+                {"relogin after" |> Utils.ste}
+              </Link.Jsx2>
+              {"." |> Utils.ste}
+            </>;
+
+          let warningNoEmail =
+            <>
+              <span className=Styles.icon>
+                <Icon.Icon icon="EMAIL_UNREAD" />
+              </span>
+              {"Please specify and vefiry your email address in Auth0.com and "
+               |> Utils.ste}
+              <Link.Jsx2 linkType={Action(_ => Auth.Actions.logout())}>
+                {"relogin after" |> Utils.ste}
+              </Link.Jsx2>
+              {"." |> Utils.ste}
+            </>;
+
+          switch (loggedInUser) {
+          | Some({
+              isEmailVerified: Some(isEmailVerified),
+              email: Some(email),
+            }) =>
+            isEmailVerified ? ReasonReact.null : warning(email)
+          | _ => warningNoEmail
+          };
+        }}
+      </Providers.AppContext.Consumer>
+    </>;
   },
 };
--- packages/client/src/lib/Icon.js
+++ packages/client/src/lib/Icon.js
@@ -1,43 +1,44 @@
-import React from "react";
-import { Icon as Iconn } from 'react-icons-kit'
-import { home } from 'react-icons-kit/typicons/home'
-import { user } from 'react-icons-kit/ikons/user'
-import { arrowLeft2 } from 'react-icons-kit/icomoon/arrowLeft2'
-import { arrowRight2 } from 'react-icons-kit/icomoon/arrowRight2'
-import { earth } from 'react-icons-kit/icomoon/earth'
-import { columns } from 'react-icons-kit/ikons/columns'
-import { ic_people } from 'react-icons-kit/md/ic_people'
-import { bulb } from 'react-icons-kit/entypo/bulb'
-import { socialBuffer } from 'react-icons-kit/ionicons/socialBuffer'
-import { flash } from 'react-icons-kit/entypo/flash'
-import { gavel } from 'react-icons-kit/fa/gavel'
-import { plus as circlePlus } from 'react-icons-kit/metrize/plus'
-import { thinRight } from 'react-icons-kit/entypo/thinRight'
-import { thinLeft } from 'react-icons-kit/entypo/thinLeft'
-import { lock } from 'react-icons-kit/icomoon/lock'
-import { chevronDown } from 'react-icons-kit/fa/chevronDown'
-import { u26FA as tent } from 'react-icons-kit/noto_emoji_regular/u26FA'
-
+import React from 'react';
+import { Icon as Iconn } from 'react-icons-kit';
+import { home } from 'react-icons-kit/typicons/home';
+import { user } from 'react-icons-kit/ikons/user';
+import { arrowLeft2 } from 'react-icons-kit/icomoon/arrowLeft2';
+import { arrowRight2 } from 'react-icons-kit/icomoon/arrowRight2';
+import { earth } from 'react-icons-kit/icomoon/earth';
+import { columns } from 'react-icons-kit/ikons/columns';
+import { ic_people } from 'react-icons-kit/md/ic_people';
+import { bulb } from 'react-icons-kit/entypo/bulb';
+import { socialBuffer } from 'react-icons-kit/ionicons/socialBuffer';
+import { flash } from 'react-icons-kit/entypo/flash';
+import { gavel } from 'react-icons-kit/fa/gavel';
+import { plus as circlePlus } from 'react-icons-kit/metrize/plus';
+import { thinRight } from 'react-icons-kit/entypo/thinRight';
+import { thinLeft } from 'react-icons-kit/entypo/thinLeft';
+import { lock } from 'react-icons-kit/icomoon/lock';
+import { chevronDown } from 'react-icons-kit/fa/chevronDown';
+import { emailUnread } from 'react-icons-kit/ionicons/emailUnread';
+import { u26FA as tent } from 'react-icons-kit/noto_emoji_regular/u26FA';
 
 let types = {
-  "HOME": home,
-  "LOCK": lock,
-  "USER": user,
-  "COLUMNS": columns,
-  "EARTH": earth,
-  "TENT": tent,
-  "LAYERS": socialBuffer,
-  "PEOPLE": ic_people,
-  "FLASH": flash,
-  "GAVEL": gavel,
-  "BULB": bulb,
-  "CIRCLE_PLUS": circlePlus,
-  "ARROW_RIGHT": arrowRight2,
-  "ARROW_LEFT": arrowLeft2,
-  "CHEVRON_LEFT": thinLeft,
-  "CHEVRON_RIGHT": thinRight,
-  "CHEVRON_DOWN": chevronDown,
-  "THIN_RIGHT": thinRight
+  'HOME': home,
+  'LOCK': lock,
+  'USER': user,
+  'COLUMNS': columns,
+  'EARTH': earth,
+  'TENT': tent,
+  'LAYERS': socialBuffer,
+  'PEOPLE': ic_people,
+  'FLASH': flash,
+  'GAVEL': gavel,
+  'BULB': bulb,
+  'CIRCLE_PLUS': circlePlus,
+  'ARROW_RIGHT': arrowRight2,
+  'ARROW_LEFT': arrowLeft2,
+  'CHEVRON_LEFT': thinLeft,
+  'CHEVRON_RIGHT': thinRight,
+  'CHEVRON_DOWN': chevronDown,
+  'THIN_RIGHT': thinRight,
+  'EMAIL_UNREAD': emailUnread,
 };
 
 export class Icon extends React.Component {
@@ -46,6 +47,6 @@ export class Icon extends React.Component {
       iconType,
       size
     } = this.props;
-    return (<Iconn size={size} icon={types[iconType]}/>)
+    return (<Iconn size={size} icon={types[iconType]}/>);
   }
 }

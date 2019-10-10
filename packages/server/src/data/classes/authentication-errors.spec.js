a902d4328ecc222246d31f056af0a84e8e66e7a3
RomanGalochkin <roman.redrat@gmail.com>
2019-10-10 08:58:35 +0300

    Adds skeletons for tests

---
+++ packages/server/src/data/classes/authentication-errors.spec.js
@@ -0,0 +1,7 @@
+const index = require('./authentication-errors');
+
+describe('Authentication Errors', () => {
+  it('returns an object', () => {
+    expect(index).toBeInstanceOf(Object);
+  });
+});
---
+++ packages/server/src/data/classes/index.spec.js
@@ -0,0 +1,7 @@
+const index = require('./index');
+
+describe('Data Classes Index File', () => {
+  it('returns an object', () => {
+    expect(index).toBeInstanceOf(Object);
+  });
+});

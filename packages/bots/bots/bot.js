9811354ac625d63e1c2db93a5e465c83f138c88d
RomanGalochkin <roman.redrat@gmail.com>
2019-10-18 09:17:32 +0300

    Adds mutex

--- packages/bots/bots/bot.js
+++ packages/bots/bots/bot.js
@@ -9,15 +9,7 @@ const { Aggregation } = require('./aggregation');
 class Bot {
   constructor() {
     this.api = new API(config.BOT_TOKEN);
-  }
-
-  /**
-   * @protected
-   * @return {Promise<string>}
-   */
-  async _getAgentId() {
-    const authenticated = await this.api.queryAuthenticated();
-    return _.get(authenticated, 'agent.id');
+    this.mutex = null;
   }
 
   /**
@@ -27,6 +19,9 @@ class Bot {
   async main() {
     const agentId = await this._getAgentId();
     assert(!_.isEmpty(agentId), 'Agent ID is required.');
+
+    await this.mutexTake();
+    assert(this.mutex !== null, 'Mutex is not free.');
 
     const measurementsNotTagged = await this.api.measurementsCompetitive({
       notTaggedByAgent: agentId,
@@ -44,70 +39,125 @@ class Bot {
     );
 
     for (const measurement of measurementsNotTagged) {
-
-      console.log(`\x1b[43m --- \x1b[0m`);
-
-      const measurableId = measurement.measurableId;
-      const createdAt = measurement.createdAt;
-      const relevantAt = measurement.createdAt;
-      const taggedMeasurementId = measurement.id;
-
-      console.log(
-        `\x1b[43mMeasurable id = "${measurableId}", ` +
-        `created at = "${createdAt}".\x1b[0m`
-      );
-
-      const measurements = await this.api.measurementsCompetitive({
-        measurableId,
-        findInDateRange: { endDate: createdAt },
-      });
-
-      const measurementsInOrder = _.orderBy(
-        measurements,
-        ['createdAt'],
-        ['desc'],
-      );
-      const lastMeasurementsOfEachAgent = _.uniqBy(
-        measurementsInOrder,
-        r => r.agentId,
-      );
-
-      console.log(
-        `\x1b[43mGot "${measurementsInOrder.length}" after sorting.\x1b[0m`,
-      );
-      console.log(
-        `\x1b[43mGot "${lastMeasurementsOfEachAgent.length}"`
-        + ` for aggregation.\x1b[0m`
-      );
-
-      if (lastMeasurementsOfEachAgent.length === 0) {
-        continue;
-      }
-
-      const aggregation = new Aggregation(lastMeasurementsOfEachAgent);
-      const aggregated = await aggregation.main();
-      if (!aggregated) {
-        console.log(`\x1b[43mNothing to aggregate.\x1b[0m`);
-        continue;
-      }
-
-      const measurementIds = lastMeasurementsOfEachAgent.map(item => item.id);
-
-      console.log(
-        `\x1b[43mMeasurement IDs "${measurementIds.join(', ')}".\x1b[0m`,
-      );
-
-      await this.api.measurementCreateAggregation({
-        measurableId,
-        relevantAt,
-        taggedMeasurementId,
-        ...aggregated
-      });
+      await this.aggregate(measurement);
     }
 
+    await this.mutexFree();
     return true;
   }
 
+  /**
+   * @returns {Promise<void>}
+   */
+  async mutexTake() {
+    for (let i = 0, max = 5; i < max; i++) {
+      this.mutex = await this.api.mutexTake();
+      if (this.mutex) {
+        break;
+      }
+      await this.sleep(3000);
+    }
+  }
+
+  /**
+   * @returns {Promise<void>}
+   */
+  async mutexFree() {
+    await this.api.mutexFree(this.mutex.id);
+  }
+
+  /**
+   * @param measurement
+   * @returns {Promise<void>}
+   */
+  async aggregate(measurement) {
+    console.log(`\x1b[43m --- \x1b[0m`);
+
+    const measurableId = measurement.measurableId;
+    const createdAt = measurement.createdAt;
+    const relevantAt = measurement.createdAt;
+    const taggedMeasurementId = measurement.id;
+
+    console.log(
+      `\x1b[43mMeasurable id = "${measurableId}", ` +
+      `created at = "${createdAt}".\x1b[0m`
+    );
+
+    const lastMeasurementsByAgent
+      = await this.getLastMeasurementsByAgent(measurableId, createdAt);
+
+    if (lastMeasurementsByAgent.length === 0) {
+      return;
+    }
+
+    const aggregated = await new Aggregation(lastMeasurementsByAgent).main();
+    if (!aggregated) {
+      console.log(`\x1b[43mNothing to aggregate.\x1b[0m`);
+      return;
+    }
+
+    const measurementIds = lastMeasurementsByAgent.map(item => item.id);
+
+    console.log(
+      `\x1b[43mMeasurement IDs "${measurementIds.join(', ')}".\x1b[0m`,
+    );
+
+    await this.api.measurementCreateAggregation({
+      measurableId,
+      relevantAt,
+      taggedMeasurementId,
+      ...aggregated
+    });
+  }
+
+  /**
+   * @param {string }measurableId
+   * @param {string} createdAt
+   * @returns {Promise<Array>}
+   */
+  async getLastMeasurementsByAgent(measurableId, createdAt) {
+    const measurements = await this.api.measurementsCompetitive({
+      measurableId,
+      findInDateRange: { endDate: createdAt },
+    });
+
+    const measurementsInOrder = _.orderBy(
+      measurements,
+      ['createdAt'],
+      ['desc'],
+    );
+    const lastMeasurementsByAgent = _.uniqBy(
+      measurementsInOrder,
+      r => r.agentId,
+    );
+
+    console.log(
+      `\x1b[43mGot "${measurementsInOrder.length}" after sorting.\x1b[0m`,
+    );
+    console.log(
+      `\x1b[43mGot "${lastMeasurementsByAgent.length}"`
+      + ` for aggregation.\x1b[0m`
+    );
+
+    return lastMeasurementsByAgent;
+  }
+
+  /**
+   * @protected
+   * @return {Promise<string>}
+   */
+  async _getAgentId() {
+    const authenticated = await this.api.queryAuthenticated();
+    return _.get(authenticated, 'agent.id');
+  }
+
+  /**
+   * @param ms
+   * @returns {Promise<*>}
+   */
+  sleep(ms = 3000) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 }
 
 module.exports = {

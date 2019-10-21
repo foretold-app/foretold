71cf892b2da10810ed7e4d527ea4278d44a988e9
Roman Galochkin <roman.redrat@gmail.com>
2019-10-21 10:21:58 +0300

    Adds authorizer

--- packages/server/src/graphql/authorizers/rate-limit.js
+++ packages/server/src/graphql/authorizers/rate-limit.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
-const {rule} = require('graphql-shield');
-const {getGraphQLRateLimiter} = require('graphql-rate-limit');
+const { rule } = require('graphql-shield');
+const { getGraphQLRateLimiter } = require('graphql-rate-limit');
 
 const logger = require('../../lib/log');
 
@@ -8,11 +8,12 @@ const log = logger.module('authorizers/rate-limit');
 
 const rateLimiter = getGraphQLRateLimiter({
   identifyContext: (context) => {
-    return _.get(context, 'agent.id');
+    return _.get(context, 'ip');
   }
 });
 
 /**
+ * Here we need to make empiric tests.
  * @param {*} root
  * @param {object} args
  * @param {Schema.Context} context
@@ -21,14 +22,16 @@ const rateLimiter = getGraphQLRateLimiter({
  */
 async function rateLimitRule(root, args, context, info) {
   const errorMessage = await rateLimiter(
-    {parent: root, args, context, info},
-    {max: 2, window: '10s'}
+    { parent: root, args, context, info },
+    { max: 50, window: '1s' }
   );
 
-  log.trace('\x1b[33m Rule Rate Limit (rateLimitRule) '
-    + `result = "${errorMessage}".\x1b[0m`);
+  const result = !!!errorMessage;
 
-  return !!errorMessage;
+  log.trace('\x1b[33m Rule Rate Limit (rateLimitRule) '
+    + `result = "${result}".\x1b[0m`);
+
+  return result;
 }
 
 /** @type {Rule} */

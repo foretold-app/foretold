1e03f868dce64149dbfa6365e385ac2c078f744b
RomanGalochkin <roman.redrat@gmail.com>
2019-10-30 13:02:51 +0300

    Reduces eslint warnings

--- packages/server/src/graphql/types/permissions.js
+++ packages/server/src/graphql/types/permissions.js
@@ -3,16 +3,32 @@ const graphql = require('graphql');
 const permissionQueries = new graphql.GraphQLObjectType({
   name: 'PermissionQueries',
   fields: () => ({
-    allow: { type: graphql.GraphQLNonNull(graphql.GraphQLList(require('./queries').queries)) },
-    deny: { type: graphql.GraphQLNonNull(graphql.GraphQLList(require('./queries').queries)) },
+    allow: {
+      type: graphql.GraphQLNonNull(
+        graphql.GraphQLList(require('./queries').queries),
+      ),
+    },
+    deny: {
+      type: graphql.GraphQLNonNull(
+        graphql.GraphQLList(require('./queries').queries),
+      ),
+    },
   }),
 });
 
 const permissionMutations = new graphql.GraphQLObjectType({
   name: 'PermissionMutations',
   fields: () => ({
-    allow: { type: graphql.GraphQLNonNull(graphql.GraphQLList(require('./mutations').mutations)) },
-    deny: { type: graphql.GraphQLNonNull(graphql.GraphQLList(require('./mutations').mutations)) },
+    allow: {
+      type: graphql.GraphQLNonNull(
+        graphql.GraphQLList(require('./mutations').mutations),
+      ),
+    },
+    deny: {
+      type: graphql.GraphQLNonNull(
+        graphql.GraphQLList(require('./mutations').mutations),
+      ),
+    },
   }),
 });
 

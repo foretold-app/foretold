e05501af8b92130c177b3b67ab0bd94752a7eb4e
Nuño Sempere <nuno.sempere@gmail.com>
2019-11-04 17:22:35 +0100

    Update Function.js

--- packages/guesstimator/formatter/formatters/Function.js
+++ packages/guesstimator/formatter/formatters/Function.js
@@ -1,8 +1,10 @@
 import _ from "lodash";
+import {regexNumberToNumberIntoLognormal} from './lib'
+
 //FIXME
 export const item = {
   formatterName: 'FUNCTION',
   matches({text}) { return !!text && text.startsWith('=') },
   error({text}) { return !_.isEmpty(text.slice(1)) ? {} : {type: 1, subType: 2}},
-  format({text}) { return {guesstimateType: 'FUNCTION', text: text.slice(1)} },
+  format({text}) { return {guesstimateType: 'FUNCTION', text: (NumberToNumberIntoLognormal(text)).slice(1)} },
 }

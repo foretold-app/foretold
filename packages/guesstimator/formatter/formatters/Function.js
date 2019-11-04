787a12b6d300d35653526630feb328a4e6c9cfa4
Nuño Sempere <nuno.sempere@gmail.com>
2019-11-04 17:34:39 +0100

    Update Function.js

--- packages/guesstimator/formatter/formatters/Function.js
+++ packages/guesstimator/formatter/formatters/Function.js
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import {regexNumberToNumberIntoLognormal} from './lib'
+import {NumberToNumberIntoLognormal} from './lib'
 
 //FIXME
 export const item = {
@@ -7,4 +7,9 @@ export const item = {
   matches({text}) { return !!text && text.startsWith('=') },
   error({text}) { return !_.isEmpty(text.slice(1)) ? {} : {type: 1, subType: 2}},
   format({text}) { return {guesstimateType: 'FUNCTION', text: (NumberToNumberIntoLognormal(text)).slice(1)} },
+      // text: (NumberToNumberIntoLognormal(text)).slice(1)
+      //        Allows the program to parse an input of the form "=mm(normal(1,10), 1 to 100)"
+      // text: text.slice(1)
+      //        Doesn't allow the program to parse an input of the form "=mm(normal(1,10), 1 to 100)"
+         
 }

26f21a7f51588efa4143dc79ce974be000179fb3
Nuño Sempere <nuno.sempere@gmail.com>
2019-11-05 14:48:25 +0100

    Update distributionMath.js

--- packages/guesstimator/lib/distributionMath.js
+++ packages/guesstimator/lib/distributionMath.js
@@ -1,5 +1,11 @@
 import math from 'mathjs'
 
+
+
+// The function: distributionUpToIntoLognormal
+// assumes a centered 90% confidence interval, e.g. the left endpoint
+// marks 0.05% on the CDF, the right 0.95%.
+
 export function distributionUpToIntoLognormal(low, high){
 
     let logHigh = math.log(high)

cf810026032cb68f05ad47bb9f82abaa993d552f
Nuño Sempere <nuno.sempere@gmail.com>
2019-11-05 14:47:39 +0100

    Update DistributionLognormal.js

--- packages/guesstimator/samplers/DistributionLognormal.js
+++ packages/guesstimator/samplers/DistributionLognormal.js
@@ -4,8 +4,6 @@ import {distributionUpToIntoLognormal} from '../lib/distributionMath.js'
 
 export var Sampler = {
   sample({params: [low, high]}, n, _1) {
-    // This assumes a centered 90% confidence interval, e.g. the left endpoint
-    // marks 0.05% on the CDF, the right 0.95%.
     return simulate(distributionUpToIntoLognormal(low, high), [], n)
   }
 }

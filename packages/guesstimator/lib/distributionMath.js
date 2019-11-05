267d059371b2218be4acfcc429c86e2d65156b53
Nuño Sempere <nuno.sempere@gmail.com>
2019-11-05 22:20:29 +0100

    Update distributionMath.js

--- packages/guesstimator/lib/distributionMath.js
+++ packages/guesstimator/lib/distributionMath.js
@@ -9,8 +9,8 @@ export function distributionUpToIntoLognormal(low, high){
     let logHigh = math.log(high)
     let logLow = math.log(low)
 
-    let mean = (math.mean(logHigh, logLow)).toFixed(3)
-    let stdev = ((logHigh-logLow) / (2*1.645)).toFixed(3)
+    let mean = (math.mean(logHigh, logLow))
+    let stdev = ((logHigh-logLow) / (2*1.645))
 
     return `lognormal(${mean},${stdev})`
 }

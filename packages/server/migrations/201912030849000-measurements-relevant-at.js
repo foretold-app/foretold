4a62550ce0467860af94bfc463742c5607d83c17
Roman Galochkin <roman.redrat@gmail.com>
2019-12-03 08:58:30 +0300

    Adds a migration to copy "createdAt" to "relevantAt" column for competitive measurements

---
+++ packages/server/migrations/201912030849000-measurements-relevant-at.js
@@ -0,0 +1,36 @@
+const { MEASUREMENT_COMPETITOR_TYPE } = require('../src/enums');
+
+module.exports = {
+  up: async function (queryInterface) {
+    try {
+      await queryInterface.sequelize.query('BEGIN');
+
+      const { COMPETITIVE } = MEASUREMENT_COMPETITOR_TYPE;
+
+      await queryInterface.sequelize.query(
+        'UPDATE "Measurements" SET "relevantAt" = "createdAt" ' +
+        `WHERE "competitorType" = '${COMPETITIVE}'`,
+      );
+
+      await queryInterface.sequelize.query('COMMIT');
+    } catch (e) {
+      console.error('Migration Up Error', e);
+      await queryInterface.sequelize.query('ROLLBACK');
+      throw e;
+    }
+  },
+
+  down: async function (queryInterface) {
+    try {
+      await queryInterface.sequelize.query('BEGIN');
+
+      // No Way Back
+
+      await queryInterface.sequelize.query('COMMIT');
+    } catch (e) {
+      console.error('Migration Down Error', e);
+      await queryInterface.sequelize.query('ROLLBACK');
+      throw e;
+    }
+  }
+};

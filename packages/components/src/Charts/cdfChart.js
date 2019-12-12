e23e6c080d1431f30863f63bea7b0916da98d1ca
Roman Galochkin <roman.redrat@gmail.com>
2019-12-12 09:49:50 +0300

    Fixes code style for cdf classes

--- packages/components/src/Charts/cdfChart.js
+++ packages/components/src/Charts/cdfChart.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import chart from "./cdfChartd3"
-import _ from "lodash"
+import _ from "lodash";
+import chart from "./cdfChartd3";
 
+/**
+ * @param min
+ * @param max
+ * @returns {number}
+ */
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Example input: {xs: [50,100,300,400,500,600], ys: [0.1, 0.4, 0.6, 0.7,0.8, 0.9]}}
+/**
+ * Example input: {
+ * xs: [50,100,300,400,500,600],
+ * ys: [0.1, 0.4, 0.6, 0.7,0.8, 0.9]},
+ * }
+ */
 class CdfChart extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      divId: "chart-" + getRandomInt(0,100000)
-    }
+      divId: "chart-" + getRandomInt(0, 100000)
+    };
   }
 
   componentDidMount() {
@@ -24,27 +34,36 @@ class CdfChart extends React.Component {
   componentDidUpdate() {
     this.drawChart();
   }
-    
+
+  /**
+   * TODO: Fix for log when minX is 0;
+   */
   drawChart() {
-    // TODO: Fix for log when minX is 0;
     var _chart = chart()
-          .svgHeight(this.props.height)
-          .maxX(this.props.maxX)
-          .marginBottom(_.isFinite(this.props.marginBottom) ?  this.props.marginBottom : 15)
-          .marginLeft(5)
-          .marginRight(5)
-          .showDistributionLines(this.props.showDistributionLines)
-          .marginTop(_.isFinite(this.props.marginTop) ? this.props.marginTop : 10)
-          .verticalLine(this.props.verticalLine)
-          .showVerticalLine(this.props.showVerticalLine)
-          .onHover(e => this.props.onHover(e))
-          .container("#" + this.state.divId);
-    _chart.data({primary: this.props.primaryDistribution}).render();
+      .svgHeight(this.props.height)
+      .maxX(this.props.maxX)
+      .marginBottom(_.isFinite(this.props.marginBottom) ? this.props.marginBottom : 15)
+      .marginLeft(5)
+      .marginRight(5)
+      .showDistributionLines(this.props.showDistributionLines)
+      .marginTop(_.isFinite(this.props.marginTop) ? this.props.marginTop : 10)
+      .verticalLine(this.props.verticalLine)
+      .showVerticalLine(this.props.showVerticalLine)
+      .onHover(e => this.props.onHover(e))
+      .container("#" + this.state.divId);
+
+    _chart.data({ primary: this.props.primaryDistribution }).render();
   }
-        
-  render(){
-    /* An untruthy width prop makes chart expand to container size */
-    return <div id={this.state.divId} style={this.props.width ? {width: this.props.width + "px"} : {}}/>
+
+  /**
+   * An untruthy width prop makes chart expand to container size.
+   * @returns {*}
+   */
+  render() {
+    return <div
+      id={this.state.divId}
+      style={this.props.width ? { width: this.props.width + "px" } : {}}
+    />
   }
 }
 

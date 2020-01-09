63323c6e4dba768b0655d96ee4cdd4ac2c178448
Roman Galochkin <roman.redrat@gmail.com>
2020-01-09 14:34:26 +0300

    Adds migration to make all "createdAt", "updatedAt" columns not nullable

---
+++ packages/server/migrations/js/202001091414000-created-at-updated-at.js
@@ -0,0 +1,54 @@
+const tables = [
+  'ChannelMemberships', 'Channels', 'FeedItems',
+  'GlobalSettings', 'Invitations', 'Mutexes',
+  'Notebooks', 'Notifications', 'NotificationStatuses',
+  'Preferences', 'Series', 'Templates',
+  'Tokens',
+];
+const columns = ['createdAt', 'updatedAt'];
+
+module.exports = {
+  up: async function (queryInterface) {
+    try {
+      await queryInterface.sequelize.query('BEGIN');
+
+      // Not Null
+      for (const table of tables) {
+        for (const column of columns) {
+          await queryInterface.sequelize.query(
+            `ALTER TABLE "${table}" ALTER COLUMN "${column}" DROP NOT NULL`,
+          );
+        }
+      }
+
+      await queryInterface.sequelize.query('COMMIT');
+    } catch (e) {
+      console.error('Migration Up Error', e);
+      await queryInterface.sequelize.query('ROLLBACK');
+      throw e;
+    }
+  },
+
+  down: async function (queryInterface) {
+    try {
+      await queryInterface.sequelize.query('BEGIN');
+
+      const tables = ['Mutexes'];
+
+      // Nullable
+      for (const table of tables) {
+        for (const column of columns) {
+          await queryInterface.sequelize.query(
+            `ALTER TABLE "${table}" ALTER COLUMN "${column}" SET NOT NULL`,
+          );
+        }
+      }
+
+      await queryInterface.sequelize.query('COMMIT');
+    } catch (e) {
+      console.error('Migration Down Error', e);
+      await queryInterface.sequelize.query('ROLLBACK');
+      throw e;
+    }
+  },
+};

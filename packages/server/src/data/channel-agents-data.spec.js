35d96d6cbdc7d16f5293e63897536d793954d827
Roman Galochkin <roman.redrat@gmail.com>
2020-01-13 15:53:47 +0300

    Fixes tests

--- packages/server/src/data/channel-agents-data.spec.js
+++ packages/server/src/data/channel-agents-data.spec.js
@@ -2,6 +2,6 @@ const data = require('./channel-agents-data');
 
 describe('Channel Agents Data', () => {
   it('class should be a constructor', () => {
-    expect(data.AgentChannelsData).toBeInstanceOf(Function);
+    expect(data.ChannelAgentsData).toBeInstanceOf(Function);
   });
 });

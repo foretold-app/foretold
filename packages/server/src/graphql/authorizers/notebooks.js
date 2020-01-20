521d84cf667fd15d7ae1b5aa26fe6dfb258024bf
Roman Galochkin <roman.redrat@gmail.com>
2020-01-20 11:00:01 +0300

    Fixes link on agent

--- packages/server/src/graphql/authorizers/notebooks.js
+++ packages/server/src/graphql/authorizers/notebooks.js
@@ -12,14 +12,14 @@ const log = logger.module('authorizers/bots');
  * @return {boolean}
  */
 function notebookIsOwnedByCurrentAgentRule(_root, _args, context, _info) {
-  const notebookAgentId = _.get(context, 'notebook.agentId', null);
+  const notebookAgentId = _.get(context, 'notebook.ownerId', null);
   const currentAgentId = _.get(context, 'agent.id', null);
 
   const result = (!!notebookAgentId && !!currentAgentId)
     && notebookAgentId === currentAgentId;
 
   log.trace(
-    '\x1b[33m Rule Bots (notebookBelongsToCurrentAgentRule), '
+    '\x1b[33m Rule Notebooks (notebookBelongsToCurrentAgentRule), '
     + `result = "${result}".\x1b[0m`,
   );
 

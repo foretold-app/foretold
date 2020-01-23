c2d151149c7d7c1aa2f01109631c1b2ed1119897
Roman Galochkin <roman.redrat@gmail.com>
2020-01-23 12:20:12 +0300

    Adds a logic

--- packages/server/src/data/votes-data.js
+++ packages/server/src/data/votes-data.js
@@ -2,6 +2,13 @@ const assert = require('assert');
 const { DataBase } = require('./data-base');
 const { VoteModel } = require('../models');
 
+const { Params } = require('./classes');
+const { Query } = require('./classes');
+const { Options } = require('./classes');
+const { Data } = require('./classes');
+
+const logger = require('../lib/log');
+
 /**
  * @implements {Layers.DataSourceLayer.DataSource}
  * @property {MutexModel} model
@@ -10,6 +17,7 @@ class VotesData extends DataBase {
   constructor() {
     super();
     this.model = new VoteModel();
+    this.log = logger.module('VotesData');
   }
 
   /**
@@ -17,9 +25,31 @@ class VotesData extends DataBase {
    * @param {Models.MeasurementID} measurementId
    * @returns {boolean}
    */
-  upvote(agentId, measurementId) {
+  async upvote(agentId, measurementId) {
     assert(!!agentId, 'Agent ID is required.');
     assert(!!measurementId, 'Measurement ID is required.');
+
+    let transaction;
+    try {
+      transaction = await this.getTransaction();
+    } catch (e) {
+      this.log.trace('upvote', e.message, e);
+      throw e;
+    }
+
+    try {
+      const vote = await this._getVote(agentId, measurementId, transaction);
+
+      let voteAmount = vote.voteAmount;
+      if (voteAmount < 0) voteAmount = 0;
+      if (voteAmount <= 9) voteAmount += 1;
+
+      await this._updateVote(vote.id, voteAmount, transaction);
+      await this.commit(transaction);
+    } catch (e) {
+      this.log.trace('upvote', e.message, e);
+      await this.rollback(transaction);
+    }
     return true;
   }
 
@@ -28,10 +58,62 @@ class VotesData extends DataBase {
    * @param {Models.MeasurementID} measurementId
    * @returns {boolean}
    */
-  downvote(agentId, measurementId) {
+  async downvote(agentId, measurementId) {
     assert(!!agentId, 'Agent ID is required.');
     assert(!!measurementId, 'Measurement ID is required.');
+
+    let transaction;
+    try {
+      transaction = await this.getTransaction();
+    } catch (e) {
+      this.log.trace('downvote', e.message, e);
+      throw e;
+    }
+
+    try {
+      const vote = await this._getVote(agentId, measurementId, transaction);
+
+      let voteAmount = vote.voteAmount;
+      if (voteAmount > 0) voteAmount = 0;
+      if (voteAmount >= -9) voteAmount -= 1;
+
+      await this._updateVote(vote.id, voteAmount, transaction);
+      await this.commit(transaction);
+    } catch (e) {
+      this.log.trace('downvote', e.message, e);
+      await this.rollback(transaction);
+    }
     return true;
+  }
+
+  /**
+   * @param {Models.AgentID} agentId
+   * @param {Models.MeasurementID} measurementId
+   * @param {Layers.Transaction} transaction
+   * @returns {Promise<*>}
+   * @private
+   */
+  async _getVote(agentId, measurementId, transaction) {
+    const params = new Params({ agentId, measurementId });
+    const query = new Query();
+    const lock = { level: transaction.LOCK.UPDATE };
+    const data = new Data({ agentId, measurementId });
+    const options = new Options({ transaction, lock, raw: true });
+    return this.upsertOne(params, query, data, options);
+  }
+
+  /**
+   * @param {Models.AgentID} agentId
+   * @param {number} voteAmount
+   * @param {Layers.Transaction} transaction
+   * @returns {Promise<*>}
+   * @private
+   */
+  async _updateVote(agentId, voteAmount, transaction) {
+    const params = new Params({ id: agentId });
+    const data = new Data({ voteAmount });
+    const options = new Options({ transaction, raw: true });
+    return this.updateOne(params, data, options);
   }
 }
 

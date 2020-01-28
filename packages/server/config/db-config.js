49eb5c23a15ac9f284a26bdb1cd21bf43e6a7f62
Roman Galochkin <roman.redrat@gmail.com>
2020-01-28 11:01:57 +0300

    Tests tests

--- packages/server/config/db-config.js
+++ packages/server/config/db-config.js
@@ -22,7 +22,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    use_env_variable: process.env.DB_USE_ENV_VARIABLE || 'DATABASE_URL',
+    use_env_variable: process.env.DB_USE_ENV_VARIABLE || undefined,
     dialect: 'postgres',
     logging: false,
   },

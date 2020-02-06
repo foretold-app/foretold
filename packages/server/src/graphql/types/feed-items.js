2b8ee057d0bfb6b4e429c5c6d191d71a2b1e7399
Roman Galochkin <roman.redrat@gmail.com>
2020-02-06 14:39:36 +0300

    Updates the graphql

--- packages/server/src/graphql/types/feed-items.js
+++ packages/server/src/graphql/types/feed-items.js
@@ -24,11 +24,49 @@ const feedItemBodyMeasurable = new graphql.GraphQLObjectType({
   }),
 });
 
+const feedItemBodyMeasurement = new graphql.GraphQLObjectType({
+  name: 'FeedItemBodyMeasurement',
+  fields: () => ({
+    item: { type: graphql.GraphQLNonNull(graphql.GraphQLString) },
+    description: { type: graphql.GraphQLNonNull(graphql.GraphQLString) },
+    measurableId: { type: graphql.GraphQLNonNull(graphql.GraphQLString) },
+    measurementId: { type: graphql.GraphQLNonNull(graphql.GraphQLString) },
+  }),
+});
+
+const feedItemBodyChannel = new graphql.GraphQLObjectType({
+  name: 'FeedItemBodyChannel',
+  fields: () => ({
+    item: { type: graphql.GraphQLNonNull(graphql.GraphQLString) },
+    description: { type: graphql.GraphQLNonNull(graphql.GraphQLString) },
+  }),
+});
+
+const feedItemBodyNotebook = new graphql.GraphQLObjectType({
+  name: 'FeedItemBodyNotebook',
+  fields: () => ({
+    item: { type: graphql.GraphQLNonNull(graphql.GraphQLString) },
+    description: { type: graphql.GraphQLNonNull(graphql.GraphQLString) },
+  }),
+});
+
+const feedItemBodyJoinedMember = new graphql.GraphQLObjectType({
+  name: 'FeedItemBodyJoinedMember',
+  fields: () => ({
+    item: { type: graphql.GraphQLNonNull(graphql.GraphQLString) },
+    description: { type: graphql.GraphQLNonNull(graphql.GraphQLString) },
+  }),
+});
+
 const feedItemBody = new graphql.GraphQLObjectType({
   name: 'FeedItemBody',
   fields: () => ({
     [FEED_ITEM_BODY.generic]: { type: feedItemBodyGeneric },
     [FEED_ITEM_BODY.measurable]: { type: feedItemBodyMeasurable },
+    [FEED_ITEM_BODY.measurement]: { type: feedItemBodyMeasurement },
+    [FEED_ITEM_BODY.channel]: { type: feedItemBodyChannel },
+    [FEED_ITEM_BODY.notebook]: { type: feedItemBodyNotebook },
+    [FEED_ITEM_BODY.joinedMember]: { type: feedItemBodyJoinedMember },
   }),
 });
 

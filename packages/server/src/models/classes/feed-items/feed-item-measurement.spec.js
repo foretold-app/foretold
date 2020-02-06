2b47210d7df497c145e215721ea84245a9daa884
Roman Galochkin <roman.redrat@gmail.com>
2020-02-06 15:19:03 +0300

    Adds test for imports

---
+++ packages/server/src/models/classes/feed-items/feed-item-measurement.spec.js
@@ -0,0 +1,7 @@
+const { FeedItemMeasurement } = require('./feed-item-measurement');
+
+describe('Feed Item Measurement', () => {
+  it('returns a constructor', () => {
+    expect(FeedItemMeasurement).toBeInstanceOf(Function);
+  });
+});
---
+++ packages/server/src/models/classes/feed-items/index.spec.js
@@ -0,0 +1,7 @@
+const index = require('./index');
+
+describe('Feed Items Module', () => {
+  it('returns an object', () => {
+    expect(index).toBeInstanceOf(Object);
+  });
+});

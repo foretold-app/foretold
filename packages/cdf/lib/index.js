8d7bb8546c0b005fc86832df62e5b9060ca933f1
Ozzie Gooen <ozzieagooen@gmail.com>
2020-02-17 23:04:29 +0000

    Minor update to cdf lib

--- packages/cdf/lib/index.js
+++ packages/cdf/lib/index.js
@@ -1,10 +1,12 @@
 const { Cdf } = require('./cdf');
+const { Pdf } = require('./pdf');
 const { ContinuousDistribution } = require('./continuousDistribution');
 const { ContinuousDistributionCombination } = require('./continuousDistributionCombination');
 const { scoringFunctions } = require('./scoring');
 
 module.exports = {
   Cdf,
+  Pdf,
   ContinuousDistribution,
   ContinuousDistributionCombination,
   scoringFunctions,

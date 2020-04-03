0001d870d6ded623d7d465794806b080446eb33b
Roman Galochkin <roman.redrat@gmail.com>
2020-04-03 19:26:51 +0300

    It is weird but works

--- packages/prediction-analysis/src/Measures/MeasurementValueMeasures.gen.js
+++
@@ -1,6 +0,0 @@
-/* Untyped file generated from MeasurementValueMeasures.re by genType. */
-
-const MeasurementValueMeasuresBS = require('./MeasurementValueMeasures.bs');
-
-const differentialEntropy = MeasurementValueMeasuresBS.differentialEntropy;;
-exports.differentialEntropy = differentialEntropy

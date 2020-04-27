2e9edd97693a201ddd6bb9d074d18f08e8666d81
Roman Galochkin <roman.redrat@gmail.com>
2020-04-27 09:35:36 +0300

    Fixes bs-fixer script

--- bs-fixer.sh
+++ bs-fixer.sh
@@ -1,7 +1,7 @@
-unlink ./packages/client/node_modules/@foretold/cdf
-unlink ./packages/client/node_modules/@foretold/guesstimator
-unlink ./packages/client/node_modules/@foretold/components
-unlink ./packages/client/node_modules/@foretold/bs-distplus
+rm -rf ./packages/client/node_modules/@foretold/cdf
+rm -rf ./packages/client/node_modules/@foretold/guesstimator
+rm -rf ./packages/client/node_modules/@foretold/components
+rm -rf ./packages/client/node_modules/@foretold/bs-distplus
 
 rsync -av --progress ./packages/cdf ./packages/client/node_modules/@foretold --exclude node_modules
 rsync -av --progress ./packages/guesstimator ./packages/client/node_modules/@foretold --exclude node_modules

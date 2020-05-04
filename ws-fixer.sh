e552154cc636389f895fc9c13f9ef892d2db1340
Roman Galochkin <roman.redrat@gmail.com>
2020-05-04 21:53:54 +0300

    Some fixes

--- ws-fixer.sh
+++ ws-fixer.sh
@@ -1,7 +1,11 @@
 echo "Workspace Fixer"
+
 rm -rf ./packages/bs-distplus/node_modules/bs-moment
 rm -rf ./packages/bs-distplus/node_modules/@glennsl/bs-jest
+rm -rf ./packages/client/node_modules/bs-moment
 rm -rf ./node_modules/bs-ken/node_modules/@glennsl/bs-jest
-mkdir -p ./packages/components/node_modules/react-textarea-autosize/node_modules/@babel/runtime
-rsync -av --progress ./node_modules/@babel/runtime/ ./packages/components/node_modules/react-textarea-autosize/node_modules/@babel/runtime/
+
+#mkdir -p ./packages/components/node_modules/react-textarea-autosize/node_modules/@babel/runtime
+#rsync -av --progress ./node_modules/@babel/runtime/ ./packages/components/node_modules/react-textarea-autosize/node_modules/@babel/runtime/
+
 echo "OK"

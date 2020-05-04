b1b061503ee82b62ab22a27f2601b6b6d3675f33
Roman Galochkin <roman.redrat@gmail.com>
2020-05-04 12:41:57 +0300

    Adds workspaces

--- ws-fixer.sh
+++ ws-fixer.sh
@@ -1,4 +1,7 @@
 echo "Workspace Fixer"
 rm -rf ./packages/bs-distplus/node_modules/bs-moment
 rm -rf ./packages/bs-distplus/node_modules/@glennsl/bs-jest
+rm -rf ./node_modules/bs-ken/node_modules/@glennsl/bs-jest
+mkdir -p ./packages/components/node_modules/react-textarea-autosize/node_modules/@babel/runtime
+rsync -av --progress ./node_modules/@babel/runtime/ ./packages/components/node_modules/react-textarea-autosize/node_modules/@babel/runtime/
 echo "OK"

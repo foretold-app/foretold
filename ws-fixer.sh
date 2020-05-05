75e8c9990dd7f65078e3fb2946a7463727d2a37e
Roman Galochkin <roman.redrat@gmail.com>
2020-05-05 08:27:04 +0300

    Fixes building (2)

--- ws-fixer.sh
+++ ws-fixer.sh
@@ -3,6 +3,7 @@ echo "Workspace Fixer"
 rm -rf ./packages/bs-distplus/node_modules/bs-moment
 rm -rf ./packages/bs-distplus/node_modules/@glennsl/bs-jest
 rm -rf ./packages/client/node_modules/bs-moment
+rm -rf ./packages/server/node_modules/bs-platform
 rm -rf ./node_modules/bs-ken/node_modules/@glennsl/bs-jest
 
 #mkdir -p ./packages/components/node_modules/react-textarea-autosize/node_modules/@babel/runtime

a0fe4b6313c7b06f6bcd6787dfbbcfad1744c683
Ozzie Gooen <ozzieagooen@gmail.com>
2021-03-30 08:05:38 -0700

    Tried to use ssl in production

--- packages/server/config/db-config.js
+++ packages/server/config/db-config.js
@@ -5,17 +5,20 @@ module.exports = {
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     use_env_variable: process.env.DB_USE_ENV_VARIABLE || undefined,
-    dialect: 'postgres',
-    logging: false,
+    dialect: "postgres",
+    logging: false
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    use_env_variable: process.env.DB_USE_ENV_VARIABLE || 'DATABASE_URL',
-    dialect: 'postgres',
+    use_env_variable: process.env.DB_USE_ENV_VARIABLE || "DATABASE_URL",
+    dialect: "postgres",
     logging: false,
+    dialectOptions: {
+      ssl: true
+    }
   },
   test: {
     username: process.env.DB_USERNAME,
@@ -23,7 +26,7 @@ module.exports = {
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     use_env_variable: process.env.DB_USE_ENV_VARIABLE || undefined,
-    dialect: 'postgres',
-    logging: false,
-  },
+    dialect: "postgres",
+    logging: false
+  }
 };

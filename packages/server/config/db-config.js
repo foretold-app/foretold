2f291b68efcea48d68afb2d0996945fc0affe76a
Ozzie Gooen <ozzieagooen@gmail.com>
2021-03-30 08:11:59 -0700

    Added sll rejectUnauthorized error to get by self signed certificate error

--- packages/server/config/db-config.js
+++ packages/server/config/db-config.js
@@ -17,7 +17,10 @@ module.exports = {
     dialect: "postgres",
     logging: false,
     dialectOptions: {
-      ssl: true
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
   },
   test: {
